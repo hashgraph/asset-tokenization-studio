594bb63d9c144492acaa354f3c6c293d9ae2f5b5
Alberto Molina <alberto@io.builders>
2024-04-19 11:58:23 +0200

    License contracts scripts
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- addLicence.js
+++ addLicence.js
@@ -24,4 +24,4 @@ async function prependContentToFiles(directory, contentFile, fileExtension) {
 }
 
 // Usage example (adjust the paths as needed)
-prependContentToFiles('./contracts/scripts/*', './LICENSE', 'ts');
+prependContentToFiles('./contracts/scripts/**', './LICENSE', 'ts');
