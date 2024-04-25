dec19fe73110c38d3d7503ebcf0ccc12d9da4c43
Alberto Molina <alberto@io.builders>
2024-04-25 10:43:10 +0200

    commit
    Signed-off-by: Alberto Molina <alberto@io.builders>

---
+++ changeProyectsReferencesToRepo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# example: ./changeVersionToPublish.sh 1.0.0
+
+version=$(node -p -e "require('./package.json').version")
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/securitytoken-sdk\": \".*\"/\"@hashgraph\/securitytoken-sdk\": \"$version\"/g"
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/securitytoken-contracts\": \".*\"/\"@hashgraph\/securitytoken-contracts\": \"$version\"/g" 
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/securitytoken-uicomponents\": \".*\"/\"@hashgraph\/securitytoken-uicomponents\": \"$version\"/g"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/securitytoken-sdk\": \".*\"/\"@hashgraph\/securitytoken-sdk\": \"$version\"/g"
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/securitytoken-contracts\": \".*\"/\"@hashgraph\/securitytoken-contracts\": \"$version\"/g" 
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/securitytoken-uicomponents\": \".*\"/\"@hashgraph\/securitytoken-uicomponents\": \"$version\"/g"
+elif [[ "$OSTYPE" == "msys" ]]; then
+  # Windows
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/securitytoken-sdk\": \".*\"/\"@hashgraph\/securitytoken-sdk\": \"$version\"/g"
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/securitytoken-contracts\": \".*\"/\"@hashgraph\/securitytoken-contracts\": \"$version\"/g" 
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/securitytoken-uicomponents\": \".*\"/\"@hashgraph\/securitytoken-uicomponents\": \"$version\"/g"
+fi
--- uiComponents/test-utils.d.ts
+++ uiComponents/test-utils.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="react" />
 import type { RenderResult } from "@testing-library/react";
 export declare const render: (ui: JSX.Element, theme?: Record<string, any>) => RenderResult;
