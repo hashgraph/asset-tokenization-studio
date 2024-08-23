e8fa0b252edb9e0c0cba216eefc76df52b2ff60f
Miguel_LZPF <miguel.carpena@io.builders>
2024-08-23 11:45:46 +0200

    fix: added npm install in ROOT inside install.js script & improve script
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- install.js
+++ install.js
@@ -1,12 +1,12 @@
-const { execSync } = require("node:child_process");
+const { execSync } = require("child_process");
+const path = require("path");
 
 const dir = __dirname;
-const webDir = `${dir}/web`;
-const uiComponentsDir = `${dir}/uiComponents`;
-const sdkDir = `${dir}/sdk`;
-const conDir = `${dir}/contracts`;
+const webDir = path.join(dir, "web");
+const sdkDir = path.join(dir, "sdk");
+const conDir = path.join(dir, "contracts");
 
-const handleError = (error, stdout, stderr) => {
+const handleError = (error) => {
   if (error) {
     console.error(error);
   }
@@ -14,52 +14,53 @@ const handleError = (error, stdout, stderr) => {
 
 const npmInstall = (dir, name = "module") => {
   process.stdout.write(`Installing dependencies for ${name}...`);
-  execSync(`cd ${dir} && npm ci`, handleError);
-  console.log("\tDone");
+  try {
+    execSync(`cd ${dir} && npm ci`, handleError);
+    console.log("\tDone");
+  } catch (error) {
+    console.error(`Failed to install dependencies for ${name}`);
+  }
 };
 
 const yarnInstall = (dir, name = "module") => {
   process.stdout.write(`Installing dependencies for ${name}...`);
-  execSync(`cd ${dir} && yarn install --frozen-lockfile`, handleError);
-  console.log("\tDone");
+  try {
+    execSync(`cd ${dir} && yarn install --frozen-lockfile`, handleError);
+    console.log("\tDone");
+  } catch (error) {
+    console.error(`Failed to install dependencies for ${name}`);
+  }
 };
 
-const yarnInstallPeer = (dir, name = "module") => {
-  process.stdout.write(`Installing dependencies for ${name}...`);
-  execSync(`cd ${dir} && yarn install --frozen-lockfile && yarn install-peers`, handleError);
-  console.log("\tDone");
+const npmBuild = (dir, name = "module") => {
+  process.stdout.write(`Building ${name}...`);
+  try {
+    execSync(`cd ${dir} && npm run build`, handleError);
+    console.log("\tDone");
+  } catch (error) {
+    console.error(`Failed to build ${name}`);
+  }
 };
 
-const npmBuild = (dir,name='module') =>{
-  process.stdout.write(`Build for ${name}...`);
-  execSync(`cd ${dir} && npm run build`, handleError);
-  console.log("\tDone");
-}
-
-const npmCompile = (dir,name='module') =>{
-  process.stdout.write(`Compile for ${name}...`);
-  execSync(`cd ${dir} && npm run compile:force`, handleError);
-  console.log("\tDone");
-}
-
-const npmRollup = (dir,name='module') =>{
-  process.stdout.write(`Rollup for ${name}...`);
-  execSync(`cd ${dir} && npm run rollup`, handleError);
-  console.log("\tDone");
-}
+const npmCompile = (dir, name = "module") => {
+  process.stdout.write(`Compiling ${name}...`);
+  try {
+    execSync(`cd ${dir} && npm run compile:force`, handleError);
+    console.log("\tDone");
+  } catch (error) {
+    console.error(`Failed to compile ${name}`);
+  }
+};
 
 let option = process.argv.slice(2)[0];
 
 if (option) {
-  npmInstall(`${dir}/${option}`, option.toUpperCase());
+  npmInstall(path.join(dir, option), option.toUpperCase());
 } else {
+  npmInstall(dir, "ROOT");
   npmInstall(conDir, "CONTRACTS");
   npmCompile(conDir, "CONTRACTS");
   npmInstall(sdkDir, "SDK");
   npmBuild(sdkDir, "SDK");
-  // yarnInstallPeer(uiComponentsDir, "UICOMPONENTS");
-  // npmRollup(uiComponentsDir, "UICOMPONENTS");
   yarnInstall(webDir, "WEB");
 }
-
-// npmLinkProject(cliDir);
