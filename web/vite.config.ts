872e7bae6c105dd5fb967ab4548070037e0d2fe9
Mario Francia <mario@io.builders>
2024-09-02 11:03:32 +0200

    fix: wallet connect dependencies

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,98 +6,101 @@ import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import EnvironmentPlugin from "vite-plugin-environment";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
+import * as path from "node:path";
 
-export default {
-  plugins: [
-    react(),
-    EnvironmentPlugin("all"),
-    pluginRewriteAll(),
-    {
-      name: "singleHMR",
-      handleHotUpdate({ modules }) {
-        modules.map((m) => {
-          m.importedModules = new Set();
-          m.importers = new Set();
-        });
-        return modules;
-      },
-    },
-    tsconfigPaths(),
-    {
-      name: "fix-node-globals-polyfill",
-      setup(build) {
-        build.onResolve({ filter: /util\.js/ }, ({ path }) => ({ path }));
-      },
-    },
-  ],
-  resolve: {
-    alias: {
-      // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
-      // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
-      // process and buffer are excluded because already managed
-      // by node-globals-polyfill
-      util: "util",
-      sys: "util",
-      events: "rollup-plugin-node-polyfills/polyfills/events",
-      stream: "rollup-plugin-node-polyfills/polyfills/stream",
-      path: "rollup-plugin-node-polyfills/polyfills/path",
-      querystring: "rollup-plugin-node-polyfills/polyfills/qs",
-      punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
-      url: "rollup-plugin-node-polyfills/polyfills/url",
-      /******** Remove the string decoder below *********/
-      // string_decoder: "rollup-plugin-node-polyfills/polyfills/string-decoder",
-      http: "rollup-plugin-node-polyfills/polyfills/http",
-      https: "rollup-plugin-node-polyfills/polyfills/http",
-      os: "rollup-plugin-node-polyfills/polyfills/os",
-      assert: "rollup-plugin-node-polyfills/polyfills/assert",
-      constants: "rollup-plugin-node-polyfills/polyfills/constants",
-      _stream_duplex:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
-      _stream_passthrough:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
-      _stream_readable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
-      _stream_writable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
-      _stream_transform:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
-      timers: "rollup-plugin-node-polyfills/polyfills/timers",
-      console: "rollup-plugin-node-polyfills/polyfills/console",
-      vm: "rollup-plugin-node-polyfills/polyfills/vm",
-      zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
-      tty: "rollup-plugin-node-polyfills/polyfills/tty",
-      domain: "rollup-plugin-node-polyfills/polyfills/domain",
-      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
-      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
-    },
-    dedupe: ["@emotion/react"],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      /********* New line inserted ***********/
-      // inject: ['./vite-polyfills/setImmediate.js'],
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        // Enable rollup polyfills plugin
-        // used during production bundling
-        rollupNodePolyFill(),
-      ],
-    },
-  },
+export default async () => {
+    const pluginRewriteAll = (await import("vite-plugin-rewrite-all")).default;
+
+    return {
+        plugins: [
+            react(),
+            EnvironmentPlugin("all"),
+            pluginRewriteAll(),
+            {
+                name: "singleHMR",
+                handleHotUpdate({ modules }) {
+                    modules.map((m) => {
+                        m.importedModules = new Set();
+                        m.importers = new Set();
+                    });
+                    return modules;
+                },
+            },
+            tsconfigPaths(),
+            {
+                name: "fix-node-globals-polyfill",
+                setup(build) {
+                    build.onResolve({ filter: /util\.js/ }, ({ path }) => ({ path }));
+                },
+            },
+        ],
+        resolve: {
+            alias: {
+                // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
+                // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+                // process and buffer are excluded because already managed
+                // by node-globals-polyfill
+                util: "util",
+                sys: "util",
+                events: "rollup-plugin-node-polyfills/polyfills/events",
+                stream: "rollup-plugin-node-polyfills/polyfills/stream",
+                path: "rollup-plugin-node-polyfills/polyfills/path",
+                querystring: "rollup-plugin-node-polyfills/polyfills/qs",
+                punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
+                url: "rollup-plugin-node-polyfills/polyfills/url",
+                http: "rollup-plugin-node-polyfills/polyfills/http",
+                https: "rollup-plugin-node-polyfills/polyfills/http",
+                os: "rollup-plugin-node-polyfills/polyfills/os",
+                assert: "rollup-plugin-node-polyfills/polyfills/assert",
+                constants: "rollup-plugin-node-polyfills/polyfills/constants",
+                _stream_duplex:
+                    "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
+                _stream_passthrough:
+                    "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
+                _stream_readable:
+                    "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
+                _stream_writable:
+                    "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
+                _stream_transform:
+                    "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
+                timers: "rollup-plugin-node-polyfills/polyfills/timers",
+                console: "rollup-plugin-node-polyfills/polyfills/console",
+                vm: "rollup-plugin-node-polyfills/polyfills/vm",
+                zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
+                tty: "rollup-plugin-node-polyfills/polyfills/tty",
+                domain: "rollup-plugin-node-polyfills/polyfills/domain",
+                buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+                process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+                '@walletconnect/modal': path.resolve(__dirname, '../sdk/node_modules/@walletconnect/modal'),
+
+            },
+            dedupe: ["@emotion/react"],
+        },
+        optimizeDeps: {
+            esbuildOptions: {
+                // inject: ['./vite-polyfills/setImmediate.js'],
+                // Node.js global to browser globalThis
+                define: {
+                    global: "globalThis",
+                },
+                // Enable esbuild polyfill plugins
+                plugins: [
+                    NodeGlobalsPolyfillPlugin({
+                        process: true,
+                        buffer: true,
+                    }),
+                    NodeModulesPolyfillPlugin(),
+                ],
+            },
+        },
+        build: {
+            rollupOptions: {
+                plugins: [
+                    // Enable rollup polyfills plugin
+                    // used during production bundling
+                    rollupNodePolyFill(),
+                ],
+            },
+        },
+    };
 };
