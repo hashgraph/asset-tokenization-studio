baf2530f0797b932f70b8801d4b47b374de19385
Mario Francia <mariofranciarius@gmail.com>
2024-09-11 08:36:04 +0200

    Fix: publish pipeline and sdk dependencies (#28)
    Signed-off-by: Mario Francia <mario@io.builders>
    Signed-off-by: Alberto Molina <alberto@io.builders>
    Co-authored-by: Alberto Molina <alberto@io.builders>

--- changeProyectsReferencesToRepo.sh
+++ changeProyectsReferencesToRepo.sh
@@ -5,14 +5,14 @@ version=$(node -p -e "require('./package.json').version")
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/assettokenization-sdk\": \".*\"/\"@hashgraph\/assettokenization-sdk\": \"$version\"/g"
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/assettokenization-contracts\": \".*\"/\"@hashgraph\/assettokenization-contracts\": \"$version\"/g" 
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/asset-tokenization-sdk\": \".*\"/\"@hashgraph\/asset-tokenization-sdk\": \"$version\"/g"
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/asset-tokenization-contracts\": \".*\"/\"@hashgraph\/asset-tokenization-contracts\": \"$version\"/g" 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/assettokenization-sdk\": \".*\"/\"@hashgraph\/assettokenization-sdk\": \"$version\"/g"
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/assettokenization-contracts\": \".*\"/\"@hashgraph\/assettokenization-contracts\": \"$version\"/g" 
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/asset-tokenization-sdk\": \".*\"/\"@hashgraph\/asset-tokenization-sdk\": \"$version\"/g"
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/asset-tokenization-contracts\": \".*\"/\"@hashgraph\/asset-tokenization-contracts\": \"$version\"/g" 
 elif [[ "$OSTYPE" == "msys" ]]; then
   # Windows
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/assettokenization-sdk\": \".*\"/\"@hashgraph\/assettokenization-sdk\": \"$version\"/g"
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/assettokenization-contracts\": \".*\"/\"@hashgraph\/assettokenization-contracts\": \"$version\"/g" 
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/asset-tokenization-sdk\": \".*\"/\"@hashgraph\/asset-tokenization-sdk\": \"$version\"/g"
+  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/asset-tokenization-contracts\": \".*\"/\"@hashgraph\/asset-tokenization-contracts\": \"$version\"/g" 
 fi
