880a2d19fc5444be96773f712be2c9498d93fa22
Alberto Molina <alberto@io.builders>
2024-09-16 14:42:58 +0200

    web src licence header added (tsx)
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- addLicence.js
+++ addLicence.js
@@ -32,4 +32,4 @@ async function prependContentToFiles(directory, contentFile, fileExtension) {
 }
 
 // Usage example (adjust the paths as needed)
-prependContentToFiles('./web/src/**', './LICENSE', 'ts');
+prependContentToFiles('./web/src/**', './LICENSE', 'tsx');
