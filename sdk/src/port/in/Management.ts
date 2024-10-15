4df6c06ed26722f45e4f17fa021544e6828a98d0
Mario Francia <mario@io.builders>
2024-10-15 16:39:08 +0200

    feat: missing features
    Signed-off-by: Mario Francia <mario@io.builders>

--- sdk/src/port/in/Management.ts
+++ sdk/src/port/in/Management.ts
@@ -1,19 +1,33 @@
 import UpdateConfigVersionRequest from "./request/UpdateConfigVersionRequest";
 import {LogError} from "../../core/decorator/LogErrorDecorator";
 import {handleValidation} from "./Common";
+import {
+    UpdateConfigVersionCommand
+} from "../../app/usecase/command/management/updateConfigVersion/updateConfigVersionCommand";
+import {QueryBus} from "../../core/query/QueryBus";
+import Injectable from "../../core/Injectable";
+import {CommandBus} from "../../core/command/CommandBus";
 
 interface IManagementInPort {
-    updateConfigVersion(UpdateConfigVersionRequest): Promise<void>;
+    updateConfigVersion(UpdateConfigVersionRequest): Promise<{ payload: boolean; transactionId: string }>;
 }
 
 class ManagementInPort implements IManagementInPort {
 
+    constructor(
+        private readonly queryBus: QueryBus = Injectable.resolve(QueryBus),
+        private readonly commandBus: CommandBus = Injectable.resolve(CommandBus),
+    ) {
+    }
+
     @LogError
-    async updateConfigVersion(request: UpdateConfigVersionRequest): Promise<void> {
+    async updateConfigVersion(request: UpdateConfigVersionRequest): Promise<{payload: boolean; transactionId: string }> {
         const {configVersion, securityId} = request;
         handleValidation('UpdateConfigVersionRequest', request);
 
-        return Promise.resolve();
+        return await this.commandBus.execute(
+            new UpdateConfigVersionCommand(configVersion, securityId)
+        );
     }
 }
 
