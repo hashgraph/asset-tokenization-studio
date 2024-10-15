332ab46fff80c76ad60085010ac8eeb1974dca54
Mario Francia <mario@io.builders>
2024-10-15 11:51:26 +0200

    feat: request and in port
    Signed-off-by: Mario Francia <mario@io.builders>

---
+++ sdk/src/port/in/Management.ts
@@ -0,0 +1,18 @@
+import UpdateConfigVersionRequest from "./request/UpdateConfigVersionRequest";
+import {LogError} from "../../core/decorator/LogErrorDecorator";
+import {handleValidation} from "./Common";
+
+interface IManagementInPort {
+    updateConfigVersion(UpdateConfigVersionRequest): Promise<void>;
+}
+
+class ManagementInPort implements IManagementInPort {
+
+    @LogError
+    async updateConfigVersion(request: UpdateConfigVersionRequest): Promise<void> {
+        const {configVersion, securityId} = request;
+        handleValidation('UpdateConfigVersionRequest', request);
+
+        return await
+    }
+}
