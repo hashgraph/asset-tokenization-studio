edb85b21d52012538df062519cf8f8811e07a7f8
Miguel_LZPF <miguel.carpena@io.builders>
2024-10-22 11:14:59 +0200

    feat: add MaturityDateUpdated event to IBondStorageWrapper interface
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- contracts/contracts/layer_2/interfaces/bond/IBondStorageWrapper.sol
+++ contracts/contracts/layer_2/interfaces/bond/IBondStorageWrapper.sol
@@ -216,6 +216,11 @@ interface IBondStorageWrapper {
         uint256 rate
     );
 
+    event MaturityDateUpdated(
+        address indexed bondId,
+        uint256 indexed maturityDate
+    );
+
     error CouponCreationFailed();
     error CouponFirstDateWrong();
     error CouponFrequencyWrong();
