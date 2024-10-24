191f44f316ee2deb32efdb746d2ad5653a4aca8f
Marcos Serradilla Diez <marcos@io.builders>
2024-10-24 11:05:11 +0200

    Cap now has setUnlimitedMaxSupply and setUnlimitedMaxSupplyByPartition
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>

--- contracts/contracts/layer_1/cap/Cap.sol
+++ contracts/contracts/layer_1/cap/Cap.sol
@@ -214,6 +214,7 @@ import {
 import {_CAP_RESOLVER_KEY} from '../constants/resolverKeys.sol';
 import {Common} from '../common/Common.sol';
 import {CapStorageWrapper} from './CapStorageWrapper.sol';
+import {_UINT256_MAX_VALUE} from '../constants/values.sol';
 
 contract Cap is ICap, IStaticFunctionSelectors, Common, CapStorageWrapper {
     // solhint-disable-next-line func-name-mixedcase
@@ -272,6 +273,34 @@ contract Cap is ICap, IStaticFunctionSelectors, Common, CapStorageWrapper {
         success_ = true;
     }
 
+    function setUnlimitedMaxSupply()
+        external
+        virtual
+        override
+        onlyUnpaused
+        onlyRole(_CAP_ROLE)
+        returns (bool success_)
+    {
+        _setMaxSupply(_UINT256_MAX_VALUE);
+        emit UnlimitedMaxSupplySet();
+        success_ = true;
+    }
+
+    function setUnlimitedMaxSupplyByPartition(
+        bytes32 _partition
+    )
+        external
+        virtual
+        override
+        onlyUnpaused
+        onlyRole(_CAP_ROLE)
+        returns (bool success_)
+    {
+        _setMaxSupplyByPartition(_partition, _UINT256_MAX_VALUE);
+        emit UnlimitedMaxSupplyByPartitionSet(_partition);
+        success_ = true;
+    }
+
     function getMaxSupply()
         external
         view
@@ -306,13 +335,19 @@ contract Cap is ICap, IStaticFunctionSelectors, Common, CapStorageWrapper {
         returns (bytes4[] memory staticFunctionSelectors_)
     {
         uint256 selectorIndex;
-        staticFunctionSelectors_ = new bytes4[](5);
+        staticFunctionSelectors_ = new bytes4[](7);
         staticFunctionSelectors_[selectorIndex++] = this
             .initialize_Cap
             .selector;
         staticFunctionSelectors_[selectorIndex++] = this.setMaxSupply.selector;
         staticFunctionSelectors_[selectorIndex++] = this
             .setMaxSupplyByPartition
+            .selector;
+        staticFunctionSelectors_[selectorIndex++] = this
+            .setUnlimitedMaxSupply
+            .selector;
+        staticFunctionSelectors_[selectorIndex++] = this
+            .setUnlimitedMaxSupplyByPartition
             .selector;
         staticFunctionSelectors_[selectorIndex++] = this.getMaxSupply.selector;
         staticFunctionSelectors_[selectorIndex++] = this
--- contracts/contracts/layer_1/interfaces/cap/ICap.sol
+++ contracts/contracts/layer_1/interfaces/cap/ICap.sol
@@ -212,6 +212,19 @@ interface ICap {
         uint256 maxSupply;
     }
 
+    /**
+     * @dev Emitted when the token max supply is set to unlimited
+     *
+     */
+    event UnlimitedMaxSupplySet();
+
+    /**
+     * @dev Emitted when the token max supply is set to unlimited for a partition
+     *
+     * @param partition The partition with unlimited max supply.
+     */
+    event UnlimitedMaxSupplyByPartitionSet(bytes32 partition);
+
     // solhint-disable-next-line func-name-mixedcase
     function initialize_Cap(
         uint256 maxSupply,
@@ -228,11 +241,30 @@ interface ICap {
     /**
      * @dev Set a max supply for a partition of the token
      *
+     * @param _partition to be set the max supply
+     * @param _maxSupply to set as maximum value
      * @return success_ true or false
      */
     function setMaxSupplyByPartition(
         bytes32 _partition,
         uint256 _maxSupply
+    ) external returns (bool success_);
+
+    /**
+     * @dev Set a max supply for the token to unlimited
+     *
+     * @return success_ true or false
+     */
+    function setUnlimitedMaxSupply() external returns (bool success_);
+
+    /**
+     * @dev Set unlimited max supply for a partition of the token
+     *
+     * @param _partition to be set the max supply
+     * @return success_ true or false
+     */
+    function setUnlimitedMaxSupplyByPartition(
+        bytes32 _partition
     ) external returns (bool success_);
 
     /**
