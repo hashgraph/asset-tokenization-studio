51b74bf18770b8526b58bc4611d98b01edbe0a19
manuel-campo-iobuilders <139767014+manuel-campo-iobuilders@users.noreply.github.com>
2024-10-25 05:24:16 -0300

    [BBND-328] As an authorized account I should be able to extend bond's maturity date (#89)
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    Signed-off-by: jaime-iobermudez <jaime.bermudez@io.builders>
    Signed-off-by: Manu Fernandez <manuel@io.builders>
    Co-authored-by: Miguel_LZPF <miguel.carpena@io.builders>
    Co-authored-by: jaime-iobermudez <jaime.bermudez@io.builders>
    Co-authored-by: Manu Fernández <110894694+ManuIOB@users.noreply.github.com>
    Signed-off-by: Mario Francia <mario@io.builders>

--- sdk/__tests__/jest-setup-file.ts
+++ sdk/__tests__/jest-setup-file.ts
@@ -1406,6 +1406,22 @@ jest.mock('../src/port/out/rpc/RPCTransactionAdapter', () => {
     >;
   });
 
+  singletonInstance.updateMaturityDate = jest.fn(async function (
+    security: EvmAddress,
+    _maturityDate: number,
+  ) {
+
+    bondInfo = {
+      ...bondInfo,
+      maturityDate: _maturityDate,
+    };
+
+    return { status: 'success', data: [] } as TransactionResponse<
+      string[],
+      Error
+    >;
+  });
+
   return {
     RPCTransactionAdapter: jest.fn(() => singletonInstance),
   };
