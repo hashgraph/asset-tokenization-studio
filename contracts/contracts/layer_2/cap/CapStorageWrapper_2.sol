d3dba952b5a265565f3584c3bef55ce1227924e1
Miguel_LZPF <miguel.carpena@io.builders>
2024-11-18 13:31:48 +0100

    refactor: streamline scheduled balance adjustments in CapStorageWrapper_2 for improved efficiency
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- contracts/contracts/layer_2/cap/CapStorageWrapper_2.sol
+++ contracts/contracts/layer_2/cap/CapStorageWrapper_2.sol
@@ -228,83 +228,39 @@ abstract contract CapStorageWrapper_2 is
         override
         returns (uint256 maxSupply_)
     {
-        // Get current ABAF witout scheduled tasks
         uint256 finalAbaf = _getERC1410BasicStorage_2().ABAF;
-        uint256 scheduledCount = _getScheduledBalanceAdjustmentCount();
-        if (scheduledCount > 0) {
-            // calculate pages of 50 items per page
-            uint256 pageCount = scheduledCount / 50;
-            uint256 pageCountRemainder = scheduledCount % 50;
-            if (pageCountRemainder > 0) {
-                pageCount++;
-            }
-            for (uint256 i = 0; i < pageCount; i++) {
-                // Get scheduled tasks for page
-                ScheduledTasksLib.ScheduledTask[] memory scheduledTasks;
-                if (i == pageCount - 1) {
-                    scheduledTasks = _getScheduledBalanceAdjustments(
-                        i,
-                        pageCountRemainder
-                    );
-                } else {
-                    scheduledTasks = _getScheduledBalanceAdjustments(i, 50);
-                }
-                // Within the page, iterate through the scheduled tasks
-                for (uint256 j = 0; j < scheduledTasks.length; j++) {
-                    if (
-                        scheduledTasks[j].scheduledTimestamp <=
-                        block.timestamp &&
-                        scheduledTasks[j].data.length > 0
-                    ) {
-                        bytes32 actionId = abi.decode(
-                            scheduledTasks[j].data,
-                            (bytes32)
-                        );
-                        (
-                            ,
-                            bytes memory balanceAdjustmentData
-                        ) = _getCorporateAction(actionId);
-                        if (balanceAdjustmentData.length > 0) {
-                            IEquity.ScheduledBalanceAdjustment
-                                memory balanceAdjustment = abi.decode(
-                                    balanceAdjustmentData,
-                                    (IEquity.ScheduledBalanceAdjustment)
-                                );
-                            // Update ABAF iteratively
-                            finalAbaf *= balanceAdjustment.factor;
-                        }
-                    }
-                }
-            }
-        }
+        finalAbaf = _applyScheduledBalanceAdjustments(finalAbaf);
         return _capStorage().maxSupply * finalAbaf;
     }
 
     function _getMaxSupplyByPartition(
         bytes32 partition_
     ) internal view override returns (uint256 maxSupply_) {
-        // Get current ABAF witout scheduled tasks
         uint256 finalAbaf = _getERC1410BasicStorage_2().ABAF;
+        finalAbaf = _applyScheduledBalanceAdjustments(finalAbaf);
+        uint256 factor = AdjustBalanceLib._calculateFactor(
+            finalAbaf,
+            _getERC1410BasicStorage_2().LABAF_partition[partition_]
+        );
+        return _capStorage().maxSupply * factor;
+    }
+
+    function _applyScheduledBalanceAdjustments(
+        uint256 initialAbaf
+    ) internal view returns (uint256 finalAbaf) {
+        finalAbaf = initialAbaf;
         uint256 scheduledCount = _getScheduledBalanceAdjustmentCount();
         if (scheduledCount > 0) {
-            // calculate pages of 50 items per page
-            uint256 pageCount = scheduledCount / 50;
-            uint256 pageCountRemainder = scheduledCount % 50;
-            if (pageCountRemainder > 0) {
-                pageCount++;
-            }
+            uint256 pageCount = (scheduledCount + 49) / 50;
             for (uint256 i = 0; i < pageCount; i++) {
-                // Get scheduled tasks for page
-                ScheduledTasksLib.ScheduledTask[] memory scheduledTasks;
-                if (i == pageCount - 1) {
-                    scheduledTasks = _getScheduledBalanceAdjustments(
+                uint256 itemsPerPage = (i == pageCount - 1)
+                    ? (scheduledCount % 50)
+                    : 50;
+                ScheduledTasksLib.ScheduledTask[]
+                    memory scheduledTasks = _getScheduledBalanceAdjustments(
                         i,
-                        pageCountRemainder
+                        itemsPerPage
                     );
-                } else {
-                    scheduledTasks = _getScheduledBalanceAdjustments(i, 50);
-                }
-                // Within the page, iterate through the scheduled tasks
                 for (uint256 j = 0; j < scheduledTasks.length; j++) {
                     if (
                         scheduledTasks[j].scheduledTimestamp <=
@@ -325,17 +281,11 @@ abstract contract CapStorageWrapper_2 is
                                     balanceAdjustmentData,
                                     (IEquity.ScheduledBalanceAdjustment)
                                 );
-                            // Update ABAF iteratively
                             finalAbaf *= balanceAdjustment.factor;
                         }
                     }
                 }
             }
         }
-        uint256 factor = AdjustBalanceLib._calculateFactor(
-            finalAbaf,
-            _getERC1410BasicStorage_2().LABAF_partition[partition_]
-        );
-        return _capStorage().maxSupply * factor;
     }
 }
