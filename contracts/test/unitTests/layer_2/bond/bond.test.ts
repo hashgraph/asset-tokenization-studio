dd152065e76c8094895de4f8445ed9e4338e92f0
Alberto Molina <alberto@io.builders>
2024-12-20 10:23:32 +0100

    test fixed
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- contracts/test/unitTests/layer_2/bond/bond.test.ts
+++ contracts/test/unitTests/layer_2/bond/bond.test.ts
@@ -232,7 +232,7 @@ import {
 } from '../../../../scripts/factory'
 import { grantRoleAndPauseToken } from '../../../../scripts/testCommon'
 
-const TIME = 10000
+const TIME = 20000
 const numberOfUnits = 1000
 let currentTimeInSeconds = 0
 let startingDate = 0
@@ -245,7 +245,7 @@ const countriesControlListType = true
 const listOfCountries = 'ES,FR,CH'
 const info = 'info'
 
-const TIME_2 = 20000
+const TIME_2 = 2 * TIME
 let couponRecordDateInSeconds = 0
 let couponExecutionDateInSeconds = 0
 const couponRate = 5
