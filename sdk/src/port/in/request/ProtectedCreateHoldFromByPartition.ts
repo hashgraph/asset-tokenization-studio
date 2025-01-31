0db63cda976e6931633191557bcab5298fb9decd
jaime-iobermudez <jaime.bermudez@io.builders>
2025-01-31 13:24:18 +0100

    fix: prettier
    Signed-off-by: jaime-iobermudez <jaime.bermudez@io.builders>

--- sdk/src/port/in/request/ProtectedCreateHoldFromByPartition.ts
+++ sdk/src/port/in/request/ProtectedCreateHoldFromByPartition.ts
@@ -249,7 +249,7 @@ export default class ProtectedCreateHoldByPartitionRequest extends ValidatedRequ
       escrow: Validation.checkHederaIdFormatOrEvmAddress(),
       sourceId: Validation.checkHederaIdFormatOrEvmAddress(),
       targetId: Validation.checkHederaIdFormatOrEvmAddress(),
-      nonce: Validation.checkNumber({min: 0}),
+      nonce: Validation.checkNumber({ min: 0 }),
       expirationDate: (val) => {
         return SecurityDate.checkDateTimestamp(
           parseInt(val),
