3703c6567904f8ba68d04636b34475113bdbd000
jaime-iobermudez <jaime.bermudez@io.builders>
2025-02-11 15:08:59 +0100

    fix: createConfiguration gas
    Signed-off-by: jaime-iobermudez <jaime.bermudez@io.builders>

--- contracts/scripts/constants.ts
+++ contracts/scripts/constants.ts
@@ -512,7 +512,7 @@ export const GAS_LIMIT = {
     businessLogicResolver: {
         getStaticResolverKey: 60_000,
         registerBusinessLogics: 7_800_000,
-        createConfiguration: 16_000_000,
+        createConfiguration: 15_000_000,
     },
 }
 
