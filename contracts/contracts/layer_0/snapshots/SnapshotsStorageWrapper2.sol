6ee2727697140d0829ef823fd26a2f631fa1353b
jaime-iobermudez <jaime.bermudez@io.builders>
2025-06-17 12:41:05 +0200

    feat: BBND-935 Create new functions for Token metadata (#474)
    Signed-off-by: jaime-iobermudez <jaime.bermudez@io.builders>

--- contracts/contracts/layer_0/snapshots/SnapshotsStorageWrapper2.sol
+++ contracts/contracts/layer_0/snapshots/SnapshotsStorageWrapper2.sol
@@ -209,11 +209,11 @@ pragma solidity 0.8.18;
 import {
     ISnapshotsStorageWrapper
 } from '../../layer_1/interfaces/snapshots/ISnapshotsStorageWrapper.sol';
-import {ERC20StorageWrapper2} from '../ERC1400/ERC20/ERC20StorageWrapper2.sol';
+import {ERC3643StorageWrapper} from '../ERC3643/ERC3643StorageWrapper.sol';
 
 abstract contract SnapshotsStorageWrapper2 is
     ISnapshotsStorageWrapper,
-    ERC20StorageWrapper2
+    ERC3643StorageWrapper
 {
     function _updateAbafSnapshot() internal {
         _updateSnapshot(_snapshotStorage().abafSnapshots, _getAbaf());
