a25315d1c7e4e486d8515902f2e587764036853d
Roberto Delgado <roberto@io.builders>
2025-06-23 14:04:17 +0200

    fix: selector count and prettier
    Signed-off-by: Roberto Delgado <roberto@io.builders>

--- contracts/contracts/layer_1/ERC3643/ERC3643.sol
+++ contracts/contracts/layer_1/ERC3643/ERC3643.sol
@@ -206,16 +206,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import {ERC1594StorageWrapper} from "../ERC1400/ERC1594/ERC1594StorageWrapper.sol";
-import {IERC3643} from "../interfaces/ERC3643/IERC3643.sol";
-import {ICompliance} from "../interfaces/ERC3643/ICompliance.sol";
-import {IIdentityRegistry} from "../interfaces/ERC3643/IIdentityRegistry.sol";
-import {IStaticFunctionSelectors} from "../../interfaces/resolver/resolverProxy/IStaticFunctionSelectors.sol";
-import {_ERC3643_RESOLVER_KEY} from "../constants/resolverKeys.sol";
-import {_DEFAULT_ADMIN_ROLE, _CONTROLLER_ROLE, _ISSUER_ROLE, _FREEZE_MANAGER_ROLE, _AGENT_ROLE} from "../constants/roles.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {IKyc} from "../interfaces/kyc/IKyc.sol";
-import {_DEFAULT_PARTITION} from "../../layer_0/constants/values.sol";
+import {
+    ERC1594StorageWrapper
+} from '../ERC1400/ERC1594/ERC1594StorageWrapper.sol';
+import {IERC3643} from '../interfaces/ERC3643/IERC3643.sol';
+import {ICompliance} from '../interfaces/ERC3643/ICompliance.sol';
+import {IIdentityRegistry} from '../interfaces/ERC3643/IIdentityRegistry.sol';
+import {
+    IStaticFunctionSelectors
+} from '../../interfaces/resolver/resolverProxy/IStaticFunctionSelectors.sol';
+import {_ERC3643_RESOLVER_KEY} from '../constants/resolverKeys.sol';
+import {
+    _DEFAULT_ADMIN_ROLE,
+    _CONTROLLER_ROLE,
+    _ISSUER_ROLE,
+    _FREEZE_MANAGER_ROLE,
+    _AGENT_ROLE
+} from '../constants/roles.sol';
+import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
+import {IKyc} from '../interfaces/kyc/IKyc.sol';
+import {_DEFAULT_PARTITION} from '../../layer_0/constants/values.sol';
 
 contract ERC3643 is IERC3643, ERC1594StorageWrapper, IStaticFunctionSelectors {
     using Strings for uint256;
@@ -330,7 +340,7 @@ contract ERC3643 is IERC3643, ERC1594StorageWrapper, IStaticFunctionSelectors {
         onlyUnProtectedPartitionsOrWildCardRole
         onlyValidKycStatus(IKyc.KycStatus.GRANTED, _userAddress)
     {
-        _redeemFrom(_userAddress, _amount, "");
+        _redeemFrom(_userAddress, _amount, '');
     }
 
     /**
@@ -352,7 +362,7 @@ contract ERC3643 is IERC3643, ERC1594StorageWrapper, IStaticFunctionSelectors {
         onlyIssuable
         onlyValidKycStatus(IKyc.KycStatus.GRANTED, _to)
     {
-        _issue(_to, _amount, "");
+        _issue(_to, _amount, '');
     }
 
     /**
@@ -380,7 +390,7 @@ contract ERC3643 is IERC3643, ERC1594StorageWrapper, IStaticFunctionSelectors {
         onlyValidKycStatus(IKyc.KycStatus.GRANTED, _to)
         returns (bool)
     {
-        _controllerTransfer(_from, _to, _amount, "", "");
+        _controllerTransfer(_from, _to, _amount, '', '');
         return true;
     }
 
@@ -539,7 +549,7 @@ contract ERC3643 is IERC3643, ERC1594StorageWrapper, IStaticFunctionSelectors {
         override
         returns (bytes4[] memory staticFunctionSelectors_)
     {
-        staticFunctionSelectors_ = new bytes4[](25);
+        staticFunctionSelectors_ = new bytes4[](24);
         uint256 selectorsIndex;
         staticFunctionSelectors_[selectorsIndex++] = this.burn.selector;
         staticFunctionSelectors_[selectorsIndex++] = this.compliance.selector;
