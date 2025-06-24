30d6ffb53145f70e5ecffd8886a78898129b2081
Axel-IoBuilders <108282711+Axel-IoBuilders@users.noreply.github.com>
2025-06-24 17:13:14 +0200

    Feat/bbnd 940 (#489)
    Signed-off-by: Axel Loupias <108282711+Axel-IoBuilders@users.noreply.github.com>

--- sdk/src/app/usecase/command/security/operations/erc3643/freezePartialTokens/FreezePartialTokensCommandHandler.ts
+++ sdk/src/app/usecase/command/security/operations/erc3643/freezePartialTokens/FreezePartialTokensCommandHandler.ts
@@ -203,171 +203,73 @@
 
 */
 
-export const COMMAND_METADATA = '__command__';
-export const COMMAND_HANDLER_METADATA = '__commandHandler__';
-export const QUERY_METADATA = '__query__';
-export const QUERY_HANDLER_METADATA = '__queryHandler__';
-export const TOKEN_CREATION_COST_HBAR = 80;
-export const EVM_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const HBAR_DECIMALS = 8;
-export const GAS = {
-  CREATE_EQUITY_ST: 15000000,
-  CREATE_BOND_ST: 15000000,
-  CASHIN: 1200000,
-  BURN: 700000,
-  WIPE: 700000,
-  RESCUE: 700000,
-  RESCUE_HBAR: 700000,
-  FREEZE: 650000,
-  UNFREEZE: 650000,
-  GRANT_KYC: 650000,
-  REVOKE_KYC: 650000,
-  REDEEM: 650000,
-  PROTECTED_REDEEM: 7000000,
-  PAUSE: 15000000,
-  UNPAUSE: 650000,
-  TAKE_SNAPSHOT: 2000000,
-  DELETE: 650000,
-  GRANT_ROLES: 2000000,
-  REVOKE_ROLES: 2000000,
-  RENOUNCE_ROLES: 2000000,
-  MAX_ROLES: 15000000,
-  INCREASE_SUPPLY: 500000,
-  DECREASE_SUPPLY: 500000,
-  RESET_SUPPLY: 450000,
-  UPDATE_RESERVE_ADDRESS: 450000,
-  UPDATE_TOKEN: 1400000,
-  UPDATE_RESERVE_AMOUNT: 400000,
-  CHANGE_PROXY_OWNER: 500000,
-  ACCEPT_PROXY_OWNER: 400000,
-  UPDATE_PROXY_IMPLEMENTATION: 400000,
-  ISSUE: 7000000,
-  MINT: 7000000,
-  CONTROLLER_TRANSFER: 7000000,
-  FORCED_TRANSFER: 7000000,
-  CONTROLLER_REDEEM: 7000000,
-  SET_DIVIDENDS: 7000000,
-  SET_VOTING_RIGHTS: 7000000,
-  SET_COUPON: 7000000,
-  SET_DOCUMENT: 7000000,
-  REMOVE_DOCUMENT: 7000000,
-  AUTHORIZE_OPERATOR: 7000000,
-  REVOKE_OPERATOR: 7000000,
-  TRANSFER_OPERATOR: 7000000,
-  TRIGGER_PENDING_SCHEDULED_SNAPSHOTS: 7000000,
-  SET_MAX_SUPPLY: 7000000,
-  PROTECT_PARTITION: 15000000,
-  UNPROTECT_PARTITION: 650000,
-  BALANCE_OF: 1200000,
-  GET_RESERVE_ADDRESS: 1200000,
-  GET_RESERVE_AMOUNT: 1200000,
-  GET_ROLES: 1200000,
-  HAS_ROLE: 1200000,
-  GET_SUPPLY_ALLOWANCE: 1200000,
-  IS_UNLIMITED_ALLOWANCE: 1200000,
-  TRANSFER: 1200000,
-  PROTECTED_TRANSFER: 7000000,
-  TRANSFER_AND_LOCK: 1200000,
-  PROTECTED_TRANSFER_AND_LOCK: 7000000,
-  ADD_TO_CONTROL_LIST: 1200000,
-  REMOVE_FROM_CONTROL_LIST: 1200000,
-  LOCK: 7000000,
-  RELEASE: 7000000,
-  EXECUTE_HOLD_BY_PARTITION: 7000000,
-  UPDATE_CONFIG_VERSION: 9000000,
-  UPDATE_CONFIG: 9000000,
-  UPDATE_RESOLVER: 9000000,
-  UPDATE_MATURITY_DATE: 7000000,
-  SET_SCHEDULED_BALANCE_ADJUSTMENT: 7000000,
-  CREATE_HOLD: 7000000,
-  CREATE_HOLD_FROM: 7000000,
-  CONTROLLER_CREATE_HOLD: 7000000,
-  PROTECTED_CREATE_HOLD: 7000000,
-  RELEASE_HOLD: 7000000,
-  RECLAIM_HOLD: 7000000,
-  ADD_ISSUER: 7000000,
-  SET_REVOCATION_REGISTRY: 7000000,
-  REMOVE_ISSUER: 7000000,
-  ACTIVATE_CLEARING: 7000000,
-  DEACTIVATE_CLEARING: 7000000,
-  CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  CLEARING_TRANSFER_FROM_BY_PARTITION: 7000000,
-  PROTECTED_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  APPROVE_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  CANCEL_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  RECLAIM_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  CLEARING_REDEEM_BY_PARTITION: 7000000,
-  CLEARING_REDEEM_FROM_BY_PARTITION: 7000000,
-  PROTECTED_CLEARING_REDEEM_BY_PARTITION: 7000000,
-  CLEARING_CREATE_HOLD_BY_PARTITION: 7000000,
-  CLEARING_CREATE_HOLD_FROM_BY_PARTITION: 7000000,
-  PROTECTED_CLEARING_CREATE_HOLD_BY_PARTITION: 7000000,
-  OPERATOR_CLEARING_CREATE_HOLD_BY_PARTITION: 7000000,
-  OPERATOR_CLEARING_REDEEM_BY_PARTITION: 7000000,
-  OPERATOR_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  UPDATE_EXTERNAL_PAUSES: 7000000,
-  ADD_EXTERNAL_PAUSE: 7000000,
-  REMOVE_EXTERNAL_PAUSE: 7000000,
-  SET_PAUSED_MOCK: 7000000,
-  CREATE_EXTERNAL_PAUSE_MOCK: 7000000,
-  UPDATE_EXTERNAL_CONTROL_LISTS: 7000000,
-  ADD_EXTERNAL_CONTROL_LIST: 7000000,
-  REMOVE_EXTERNAL_CONTROL_LIST: 7000000,
-  ACTIVATE_INTERNAL_KYC: 7000000,
-  DEACTIVATE_INTERNAL_KYC: 7000000,
-  ADD_TO_BLACK_LIST_MOCK: 7000000,
-  ADD_TO_WHITE_LIST_MOCK: 7000000,
-  REMOVE_FROM_BLACK_LIST_MOCK: 7000000,
-  REMOVE_FROM_WHITE_LIST_MOCK: 7000000,
-  CREATE_EXTERNAL_BLACK_LIST_MOCK: 7000000,
-  CREATE_EXTERNAL_WHITE_LIST_MOCK: 7000000,
-  UPDATE_EXTERNAL_KYC_LISTS: 7000000,
-  ADD_EXTERNAL_KYC_LIST: 7000000,
-  REMOVE_EXTERNAL_KYC_LIST: 7000000,
-  GRANT_KYC_MOCK: 7000000,
-  REVOKE_KYC_MOCK: 7000000,
-  CREATE_EXTERNAL_KYC_LIST_MOCK: 7000000,
-  SET_NAME: 7000000,
-  SET_SYMBOL: 7000000,
-  FREEZE_PARTIAL_TOKENS: 7000000,
-  UNFREEZE_PARTIAL_TOKENS: 7000000,
-  SET_ADDRESS_FROZEN: 700000,
-  SET_ONCHAIN_ID: 700000,
-  SET_IDENTITY_REGISTRY: 700000,
-  SET_COMPLIANCE: 700000,
-  BATCH_TRANSFER: 700000,
-  BATCH_FORCED_TRANSFER: 700000,
-  BATCH_MINT: 700000,
-  BATCH_BURN: 700000,
-  BATCH_SET_ADDRESS_FROZEN: 700000,
-  BATCH_FREEZE_PARTIAL_TOKENS: 700000,
-  BATCH_UNFREEZE_PARTIAL_TOKENS: 700000,
-  RECOVERY_ADDRESS: 7000000,
-  ADD_AGENT: 7000000,
-  REMOVE_AGENT: 7000000,
-} as const;
+import { ICommandHandler } from '../../../../../../../core/command/CommandHandler.js';
+import { CommandHandler } from '../../../../../../../core/decorator/CommandHandlerDecorator.js';
+import AccountService from '../../../../../../service/account/AccountService.js';
+import TransactionService from '../../../../../../service/transaction/TransactionService.js';
+import { lazyInject } from '../../../../../../../core/decorator/LazyInjectDecorator.js';
+import EvmAddress from '../../../../../../../domain/context/contract/EvmAddress.js';
+import { SecurityRole } from '../../../../../../../domain/context/security/SecurityRole.js';
+import ValidationService from '../../../../../../service/validation/ValidationService.js';
+import ContractService from '../../../../../../service/contract/ContractService.js';
+import { FreezePartialTokensCommandError } from './error/FreezePartialTokensCommandError.js';
+import {
+  FreezePartialTokensCommand,
+  FreezePartialTokensResponse,
+} from './FreezePartialTokensCommand.js';
+import BigDecimal from '../../../../../../../domain/context/shared/BigDecimal.js';
+import SecurityService from '../../../../../../service/security/SecurityService.js';
 
-export const SET_NAME_GAS = 7000000;
-export const SET_SYMBOL_GAS = 7000000;
-export const FREEZE_PARTIAL_TOKENS_GAS = 7000000;
-export const UNFREEZE_PARTIAL_TOKENS_GAS = 7000000;
-export const SET_ONCHAIN_ID_GAS = 700000;
-export const SET_IDENTITY_REGISTRY_GAS = 700000;
-export const SET_COMPLIANCE_GAS = 700000;
+@CommandHandler(FreezePartialTokensCommand)
+export class FreezePartialTokensCommandHandler
+  implements ICommandHandler<FreezePartialTokensCommand>
+{
+  constructor(
+    @lazyInject(SecurityService)
+    private readonly securityService: SecurityService,
+    @lazyInject(AccountService)
+    private readonly accountService: AccountService,
+    @lazyInject(TransactionService)
+    private readonly transactionService: TransactionService,
+    @lazyInject(ValidationService)
+    private readonly validationService: ValidationService,
+    @lazyInject(ContractService)
+    private readonly contractService: ContractService,
+  ) {}
 
-export const _PARTITION_ID_1 =
-  '0x0000000000000000000000000000000000000000000000000000000000000001';
-export const EMPTY_BYTES = '0x';
+  async execute(
+    command: FreezePartialTokensCommand,
+  ): Promise<FreezePartialTokensResponse> {
+    try {
+      const { securityId, amount, targetId } = command;
+      const handler = this.transactionService.getHandler();
+      const account = this.accountService.getCurrentAccount();
 
-export const SET_DIVIDEND_EVENT = 'DividendSet';
-export const SET_VOTING_RIGHTS_EVENT = 'VotingSet';
-export const SET_COUPON_EVENT = 'CouponSet';
-export const SET_SCHEDULED_BALANCE_ADJUSTMENT_EVENT =
-  'ScheduledBalanceAdjustmentSet';
+      const securityEvmAddress: EvmAddress =
+        await this.contractService.getContractEvmAddress(securityId);
 
-// * Generic
-export const BYTES_32_LENGTH = 32 * 2;
-export const ADDRESS_LENGTH = 40;
+      await this.validationService.checkPause(securityId);
+      const security = await this.securityService.get(securityId);
+      await this.validationService.checkDecimals(security, amount);
 
-// * Events from creation
-export const TOPICS_IN_FACTORY_RESULT = 6;
+      await this.validationService.checkRole(
+        SecurityRole._FREEZE_MANAGER_ROLE,
+        account.id.toString(),
+        securityId,
+      );
+
+      const res = await handler.freezePartialTokens(
+        securityEvmAddress,
+        BigDecimal.fromString(amount, security.decimals),
+        await this.accountService.getAccountEvmAddress(targetId),
+        securityId,
+      );
+
+      return Promise.resolve(
+        new FreezePartialTokensResponse(res.error === undefined, res.id!),
+      );
+    } catch (error) {
+      throw new FreezePartialTokensCommandError(error as Error);
+    }
+  }
+}
--- sdk/src/app/usecase/command/security/operations/erc3643/unfreezePartialTokens/UnfreezePartialTokensCommandHandler.unit.test.ts
+++ sdk/src/app/usecase/command/security/operations/erc3643/unfreezePartialTokens/UnfreezePartialTokensCommandHandler.unit.test.ts
@@ -203,171 +203,139 @@
 
 */
 
-export const COMMAND_METADATA = '__command__';
-export const COMMAND_HANDLER_METADATA = '__commandHandler__';
-export const QUERY_METADATA = '__query__';
-export const QUERY_HANDLER_METADATA = '__queryHandler__';
-export const TOKEN_CREATION_COST_HBAR = 80;
-export const EVM_ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-export const HBAR_DECIMALS = 8;
-export const GAS = {
-  CREATE_EQUITY_ST: 15000000,
-  CREATE_BOND_ST: 15000000,
-  CASHIN: 1200000,
-  BURN: 700000,
-  WIPE: 700000,
-  RESCUE: 700000,
-  RESCUE_HBAR: 700000,
-  FREEZE: 650000,
-  UNFREEZE: 650000,
-  GRANT_KYC: 650000,
-  REVOKE_KYC: 650000,
-  REDEEM: 650000,
-  PROTECTED_REDEEM: 7000000,
-  PAUSE: 15000000,
-  UNPAUSE: 650000,
-  TAKE_SNAPSHOT: 2000000,
-  DELETE: 650000,
-  GRANT_ROLES: 2000000,
-  REVOKE_ROLES: 2000000,
-  RENOUNCE_ROLES: 2000000,
-  MAX_ROLES: 15000000,
-  INCREASE_SUPPLY: 500000,
-  DECREASE_SUPPLY: 500000,
-  RESET_SUPPLY: 450000,
-  UPDATE_RESERVE_ADDRESS: 450000,
-  UPDATE_TOKEN: 1400000,
-  UPDATE_RESERVE_AMOUNT: 400000,
-  CHANGE_PROXY_OWNER: 500000,
-  ACCEPT_PROXY_OWNER: 400000,
-  UPDATE_PROXY_IMPLEMENTATION: 400000,
-  ISSUE: 7000000,
-  MINT: 7000000,
-  CONTROLLER_TRANSFER: 7000000,
-  FORCED_TRANSFER: 7000000,
-  CONTROLLER_REDEEM: 7000000,
-  SET_DIVIDENDS: 7000000,
-  SET_VOTING_RIGHTS: 7000000,
-  SET_COUPON: 7000000,
-  SET_DOCUMENT: 7000000,
-  REMOVE_DOCUMENT: 7000000,
-  AUTHORIZE_OPERATOR: 7000000,
-  REVOKE_OPERATOR: 7000000,
-  TRANSFER_OPERATOR: 7000000,
-  TRIGGER_PENDING_SCHEDULED_SNAPSHOTS: 7000000,
-  SET_MAX_SUPPLY: 7000000,
-  PROTECT_PARTITION: 15000000,
-  UNPROTECT_PARTITION: 650000,
-  BALANCE_OF: 1200000,
-  GET_RESERVE_ADDRESS: 1200000,
-  GET_RESERVE_AMOUNT: 1200000,
-  GET_ROLES: 1200000,
-  HAS_ROLE: 1200000,
-  GET_SUPPLY_ALLOWANCE: 1200000,
-  IS_UNLIMITED_ALLOWANCE: 1200000,
-  TRANSFER: 1200000,
-  PROTECTED_TRANSFER: 7000000,
-  TRANSFER_AND_LOCK: 1200000,
-  PROTECTED_TRANSFER_AND_LOCK: 7000000,
-  ADD_TO_CONTROL_LIST: 1200000,
-  REMOVE_FROM_CONTROL_LIST: 1200000,
-  LOCK: 7000000,
-  RELEASE: 7000000,
-  EXECUTE_HOLD_BY_PARTITION: 7000000,
-  UPDATE_CONFIG_VERSION: 9000000,
-  UPDATE_CONFIG: 9000000,
-  UPDATE_RESOLVER: 9000000,
-  UPDATE_MATURITY_DATE: 7000000,
-  SET_SCHEDULED_BALANCE_ADJUSTMENT: 7000000,
-  CREATE_HOLD: 7000000,
-  CREATE_HOLD_FROM: 7000000,
-  CONTROLLER_CREATE_HOLD: 7000000,
-  PROTECTED_CREATE_HOLD: 7000000,
-  RELEASE_HOLD: 7000000,
-  RECLAIM_HOLD: 7000000,
-  ADD_ISSUER: 7000000,
-  SET_REVOCATION_REGISTRY: 7000000,
-  REMOVE_ISSUER: 7000000,
-  ACTIVATE_CLEARING: 7000000,
-  DEACTIVATE_CLEARING: 7000000,
-  CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  CLEARING_TRANSFER_FROM_BY_PARTITION: 7000000,
-  PROTECTED_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  APPROVE_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  CANCEL_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  RECLAIM_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  CLEARING_REDEEM_BY_PARTITION: 7000000,
-  CLEARING_REDEEM_FROM_BY_PARTITION: 7000000,
-  PROTECTED_CLEARING_REDEEM_BY_PARTITION: 7000000,
-  CLEARING_CREATE_HOLD_BY_PARTITION: 7000000,
-  CLEARING_CREATE_HOLD_FROM_BY_PARTITION: 7000000,
-  PROTECTED_CLEARING_CREATE_HOLD_BY_PARTITION: 7000000,
-  OPERATOR_CLEARING_CREATE_HOLD_BY_PARTITION: 7000000,
-  OPERATOR_CLEARING_REDEEM_BY_PARTITION: 7000000,
-  OPERATOR_CLEARING_TRANSFER_BY_PARTITION: 7000000,
-  UPDATE_EXTERNAL_PAUSES: 7000000,
-  ADD_EXTERNAL_PAUSE: 7000000,
-  REMOVE_EXTERNAL_PAUSE: 7000000,
-  SET_PAUSED_MOCK: 7000000,
-  CREATE_EXTERNAL_PAUSE_MOCK: 7000000,
-  UPDATE_EXTERNAL_CONTROL_LISTS: 7000000,
-  ADD_EXTERNAL_CONTROL_LIST: 7000000,
-  REMOVE_EXTERNAL_CONTROL_LIST: 7000000,
-  ACTIVATE_INTERNAL_KYC: 7000000,
-  DEACTIVATE_INTERNAL_KYC: 7000000,
-  ADD_TO_BLACK_LIST_MOCK: 7000000,
-  ADD_TO_WHITE_LIST_MOCK: 7000000,
-  REMOVE_FROM_BLACK_LIST_MOCK: 7000000,
-  REMOVE_FROM_WHITE_LIST_MOCK: 7000000,
-  CREATE_EXTERNAL_BLACK_LIST_MOCK: 7000000,
-  CREATE_EXTERNAL_WHITE_LIST_MOCK: 7000000,
-  UPDATE_EXTERNAL_KYC_LISTS: 7000000,
-  ADD_EXTERNAL_KYC_LIST: 7000000,
-  REMOVE_EXTERNAL_KYC_LIST: 7000000,
-  GRANT_KYC_MOCK: 7000000,
-  REVOKE_KYC_MOCK: 7000000,
-  CREATE_EXTERNAL_KYC_LIST_MOCK: 7000000,
-  SET_NAME: 7000000,
-  SET_SYMBOL: 7000000,
-  FREEZE_PARTIAL_TOKENS: 7000000,
-  UNFREEZE_PARTIAL_TOKENS: 7000000,
-  SET_ADDRESS_FROZEN: 700000,
-  SET_ONCHAIN_ID: 700000,
-  SET_IDENTITY_REGISTRY: 700000,
-  SET_COMPLIANCE: 700000,
-  BATCH_TRANSFER: 700000,
-  BATCH_FORCED_TRANSFER: 700000,
-  BATCH_MINT: 700000,
-  BATCH_BURN: 700000,
-  BATCH_SET_ADDRESS_FROZEN: 700000,
-  BATCH_FREEZE_PARTIAL_TOKENS: 700000,
-  BATCH_UNFREEZE_PARTIAL_TOKENS: 700000,
-  RECOVERY_ADDRESS: 7000000,
-  ADD_AGENT: 7000000,
-  REMOVE_AGENT: 7000000,
-} as const;
+import TransactionService from '../../../../../../service/transaction/TransactionService.js';
+import { createMock } from '@golevelup/ts-jest';
+import AccountService from '../../../../../../service/account/AccountService.js';
+import {
+  ErrorMsgFixture,
+  EvmAddressPropsFixture,
+  HederaIdPropsFixture,
+  TransactionIdFixture,
+} from '../../../../../../../../__tests__/fixtures/shared/DataFixture.js';
+import ContractService from '../../../../../../service/contract/ContractService.js';
+import EvmAddress from '../../../../../../../domain/context/contract/EvmAddress.js';
+import ValidationService from '../../../../../../service/validation/ValidationService.js';
+import Account from '../../../../../../../domain/context/account/Account.js';
+import { SecurityRole } from '../../../../../../../domain/context/security/SecurityRole.js';
 
-export const SET_NAME_GAS = 7000000;
-export const SET_SYMBOL_GAS = 7000000;
-export const FREEZE_PARTIAL_TOKENS_GAS = 7000000;
-export const UNFREEZE_PARTIAL_TOKENS_GAS = 7000000;
-export const SET_ONCHAIN_ID_GAS = 700000;
-export const SET_IDENTITY_REGISTRY_GAS = 700000;
-export const SET_COMPLIANCE_GAS = 700000;
+import { ErrorCode } from '../../../../../../../core/error/BaseError.js';
+import { UnfreezePartialTokensCommandError } from './error/UnfreezePartialTokensCommandError.js';
+import {
+  UnfreezePartialTokensCommand,
+  UnfreezePartialTokensResponse,
+} from './UnfreezePartialTokensCommand.js';
+import { UnfreezePartialTokensCommandHandler } from './UnfreezePartialTokensCommandHandler.js';
+import { UnfreezePartialTokensCommandFixture } from '../../../../../../../../__tests__/fixtures/erc3643/ERC3643Fixture.js';
+import BigDecimal from '../../../../../../../domain/context/shared/BigDecimal.js';
+import SecurityService from '../../../../../../service/security/SecurityService.js';
+import { SecurityPropsFixture } from '../../../../../../../../__tests__/fixtures/shared/SecurityFixture.js';
+import { Security } from '../../../../../../../domain/context/security/Security.js';
 
-export const _PARTITION_ID_1 =
-  '0x0000000000000000000000000000000000000000000000000000000000000001';
-export const EMPTY_BYTES = '0x';
+describe('UnfreezePartialTokensCommandHandler', () => {
+  let handler: UnfreezePartialTokensCommandHandler;
+  let command: UnfreezePartialTokensCommand;
 
-export const SET_DIVIDEND_EVENT = 'DividendSet';
-export const SET_VOTING_RIGHTS_EVENT = 'VotingSet';
-export const SET_COUPON_EVENT = 'CouponSet';
-export const SET_SCHEDULED_BALANCE_ADJUSTMENT_EVENT =
-  'ScheduledBalanceAdjustmentSet';
+  const transactionServiceMock = createMock<TransactionService>();
+  const validationServiceMock = createMock<ValidationService>();
+  const accountServiceMock = createMock<AccountService>();
+  const contractServiceMock = createMock<ContractService>();
+  const securityServiceMock = createMock<SecurityService>();
 
-// * Generic
-export const BYTES_32_LENGTH = 32 * 2;
-export const ADDRESS_LENGTH = 40;
+  const evmAddress = new EvmAddress(EvmAddressPropsFixture.create().value);
+  const account = new Account({
+    id: HederaIdPropsFixture.create().value,
+    evmAddress: EvmAddressPropsFixture.create().value,
+  });
+  const transactionId = TransactionIdFixture.create().id;
+  const errorMsg = ErrorMsgFixture.create().msg;
+  const security = new Security(SecurityPropsFixture.create());
 
-// * Events from creation
-export const TOPICS_IN_FACTORY_RESULT = 6;
+  beforeEach(() => {
+    handler = new UnfreezePartialTokensCommandHandler(
+      securityServiceMock,
+      accountServiceMock,
+      transactionServiceMock,
+      validationServiceMock,
+      contractServiceMock,
+    );
+    command = UnfreezePartialTokensCommandFixture.create();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('execute', () => {
+    it('throws UnfreezePartialTokensCommandError when command fails with uncaught error', async () => {
+      const fakeError = new Error(errorMsg);
+
+      contractServiceMock.getContractEvmAddress.mockRejectedValue(fakeError);
+
+      const resultPromise = handler.execute(command);
+
+      await expect(resultPromise).rejects.toBeInstanceOf(
+        UnfreezePartialTokensCommandError,
+      );
+      await expect(resultPromise).rejects.toMatchObject({
+        message: expect.stringContaining(
+          `An error occurred while unfreeze partial tokens: ${errorMsg}`,
+        ),
+        errorCode: ErrorCode.UncaughtCommandError,
+      });
+    });
+
+    it('should successfully Unfreeze amount of tokens', async () => {
+      contractServiceMock.getContractEvmAddress.mockResolvedValueOnce(
+        evmAddress,
+      );
+      accountServiceMock.getAccountEvmAddress.mockResolvedValueOnce(evmAddress);
+      accountServiceMock.getCurrentAccount.mockReturnValue(account);
+      validationServiceMock.checkPause.mockResolvedValue(undefined);
+      validationServiceMock.checkRole.mockResolvedValue(undefined);
+      validationServiceMock.checkDecimals.mockResolvedValue(undefined);
+      securityServiceMock.get.mockResolvedValue(security);
+      transactionServiceMock
+        .getHandler()
+        .unfreezePartialTokens.mockResolvedValue({
+          id: transactionId,
+        });
+
+      const result = await handler.execute(command);
+
+      expect(result).toBeInstanceOf(UnfreezePartialTokensResponse);
+      expect(result.payload).toBe(true);
+      expect(result.transactionId).toBe(transactionId);
+
+      expect(contractServiceMock.getContractEvmAddress).toHaveBeenCalledTimes(
+        1,
+      );
+      expect(validationServiceMock.checkPause).toHaveBeenCalledTimes(1);
+      expect(validationServiceMock.checkRole).toHaveBeenCalledTimes(1);
+      expect(accountServiceMock.getCurrentAccount).toHaveBeenCalledTimes(1);
+      expect(
+        transactionServiceMock.getHandler().unfreezePartialTokens,
+      ).toHaveBeenCalledTimes(1);
+
+      expect(validationServiceMock.checkPause).toHaveBeenCalledWith(
+        command.securityId,
+      );
+      expect(validationServiceMock.checkRole).toHaveBeenCalledWith(
+        SecurityRole._FREEZE_MANAGER_ROLE,
+        account.id.toString(),
+        command.securityId,
+      );
+      expect(contractServiceMock.getContractEvmAddress).toHaveBeenCalledWith(
+        command.securityId,
+      );
+
+      expect(
+        transactionServiceMock.getHandler().unfreezePartialTokens,
+      ).toHaveBeenCalledWith(
+        evmAddress,
+        BigDecimal.fromString(command.amount, security.decimals),
+        evmAddress,
+        command.securityId,
+      );
+    });
+  });
+});
