9908e19610651142e12ea0f684af25c73af72a96
Alberto Molina <alberto@io.builders>
2025-07-11 11:26:41 +0200

    BondUSA and EquityUSA updated
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- contracts/contracts/layer_3/equityUSA/EquityUSA.sol
+++ contracts/contracts/layer_3/equityUSA/EquityUSA.sol
@@ -245,7 +245,7 @@ contract EquityUSA is IEquityUSA, Equity, Security {
         returns (bytes4[] memory staticFunctionSelectors_)
     {
         uint256 selectorIndex;
-        staticFunctionSelectors_ = new bytes4[](14);
+        staticFunctionSelectors_ = new bytes4[](16);
         staticFunctionSelectors_[selectorIndex++] = this
             ._initialize_equityUSA
             .selector;
@@ -277,6 +277,12 @@ contract EquityUSA is IEquityUSA, Equity, Security {
             .selector;
         staticFunctionSelectors_[selectorIndex++] = this
             .getSecurityRegulationData
+            .selector;
+        staticFunctionSelectors_[selectorIndex++] = this
+            .getSecurityHolders
+            .selector;
+        staticFunctionSelectors_[selectorIndex++] = this
+            .getTotalSecurityHolders
             .selector;
     }
 
