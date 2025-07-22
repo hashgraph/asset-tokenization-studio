6f2659976b5d06cf78667c15d62ebb7f6c0fff55
Roberto Delgado <roberto@io.builders>
2025-07-22 12:43:18 +0200

    refator: separate handlers tokens from injectable
    Signed-off-by: Roberto Delgado <roberto@io.builders>

--- sdk/src/core/injectable/Handlers.ts
+++ sdk/src/core/injectable/Handlers.ts
@@ -203,129 +203,174 @@
 
 */
 
-import {
-  registry,
-  container,
-  InjectionToken,
-  ValueProvider,
-  DependencyContainer,
-  delay,
-} from 'tsyringe';
 import { RPCTransactionAdapter } from '@port/out/rpc/RPCTransactionAdapter';
-import { Constructor } from '../Type';
-import { WalletEvents } from '@service/event/WalletEvent';
-import { CommandHandlerType } from '../command/CommandBus';
-import { QueryHandlerType } from '../query/QueryBus';
-import { NetworkProps } from '@service/network/NetworkService';
-import TransactionAdapter from '@port/out/TransactionAdapter';
-import { RuntimeError } from '../error/RuntimeError';
-import { SDK } from '@port/in/Common';
 import { HederaWalletConnectTransactionAdapter } from '@port/out/hs/hederawalletconnect/HederaWalletConnectTransactionAdapter';
 import { DFNSTransactionAdapter } from '@port/out/hs/hts/custodial/DFNSTransactionAdapter';
 import { FireblocksTransactionAdapter } from '@port/out/hs/hts/custodial/FireblocksTransactionAdapter';
 import { AWSKMSTransactionAdapter } from '@port/out/hs/hts/custodial/AWSKMSTransactionAdapter';
-import { TOKENS } from './Tokens';
+import { COMMAND_HANDLERS_AGENT } from './agent/InjectableAgent';
 import {
-  COMMAND_HANDLERS,
-  QUERY_HANDLERS,
-  TRANSACTION_HANDLER,
-} from './Handlers';
+  COMMAND_HANDLERS_BOND,
+  QUERY_HANDLERS_BOND,
+} from './bond/InjectableBond.js';
+import {
+  COMMAND_HANDLERS_CLEARING,
+  QUERY_HANDLERS_CLEARING,
+} from './clearing/InjectableClearing.js';
+import {
+  COMMAND_HANDLERS_COMPLIANCE,
+  QUERY_HANDLERS_COMPLIANCE,
+} from './compliance/InjectableCompliance.js';
+import {
+  COMMAND_HANDLERS_CONTROL_LIST,
+  QUERY_HANDLERS_CONTROL_LIST,
+} from './controlList/InjectableControlList.js';
+import {
+  COMMAND_HANDLERS_EQUITY,
+  QUERY_HANDLERS_EQUITY,
+} from './equity/InjectableEquity.js';
+import {
+  COMMAND_HANDLERS_FREEZE,
+  QUERY_HANDLERS_FREEZE,
+} from './freeze/InjectableFreeze.js';
+import {
+  COMMAND_HANDLERS_HOLD,
+  QUERY_HANDLERS_HOLD,
+} from './hold/InjectableHold.js';
+import {
+  COMMAND_HANDLERS_IDENTITY,
+  QUERY_HANDLERS_IDENTITY,
+} from './identity/InjectableIdentity.js';
+import { COMMAND_HANDLERS_ISSUE } from './issue/InjectableIssue.js';
+import {
+  COMMAND_HANDLERS_KYC,
+  QUERY_HANDLERS_KYC,
+} from './kyc/InjectableKyc.js';
+import {
+  COMMAND_HANDLERS_LOCK,
+  QUERY_HANDLERS_LOCK,
+} from './lock/InjectableLock.js';
+import {
+  COMMAND_HANDLERS_MOCK,
+  QUERY_HANDLERS_MOCK,
+} from './mock/InjectableMock.js';
+import { COMMAND_HANDLERS_NETWORK } from './network/InjectableNetwork.js';
+import {
+  COMMAND_HANDLERS_PAUSE,
+  QUERY_HANDLERS_PAUSE,
+} from './pause/InjectablePause.js';
+import {
+  COMMAND_HANDLERS_PROTECTED,
+  QUERY_HANDLERS_PROTECTED,
+} from './protectedPartitions/InjectableProtected.js';
+import {
+  COMMAND_HANDLERS_RBAC,
+  QUERY_HANDLERS_RBAC,
+} from './rbac/InjectableRbac.js';
+import {
+  COMMAND_HANDLERS_RECOVERY,
+  QUERY_HANDLERS_RECOVERY,
+} from './recovery/InjectableRecovery.js';
+import {
+  COMMAND_HANDLERS_REDEEM,
+  QUERY_HANDLERS_REDEEM,
+} from './redeem/InjectableRedeem.js';
+import {
+  COMMAND_HANDLERS_RESOLVER,
+  QUERY_HANDLERS_RESOLVER,
+} from './resolver/InjectableResolver.js';
+import { COMMAND_HANDLERS_METADATA } from './security/InjectableMetadata.js';
+import { COMMAND_HANDLERS_SNAPSHOT } from './snapshot/InjectableSnapshot.js';
+import {
+  COMMAND_HANDLERS_SUPPLY,
+  QUERY_HANDLERS_SUPPLY,
+} from './supply/InjectableSupply.js';
+import {
+  COMMAND_HANDLERS_TRANSFER,
+  QUERY_HANDLERS_TRANSFER,
+} from './transfer/InjectableTransfer.js';
+import { TOKENS } from './Tokens';
+import { QUERY_HANDLERS_SECURITY_DETAILS } from './security/InjectableSecurityDetails';
+import {
+  COMMAND_HANDLERS_BALANCE,
+  QUERY_HANDLERS_BALANCE,
+} from './balance/InjectableBalance';
+import { QUERY_HANDLERS_ACCOUNT } from './account/InjectableAccount';
+import { QUERY_HANDLERS_OPERATOR } from './operator/InjectableOperator';
 
-const defaultNetworkProps: NetworkProps = {
-  environment: 'testnet',
-  mirrorNode: {
-    name: 'default',
-    baseUrl: 'https://testnet.mirrornode.hedera.com',
+export const COMMAND_HANDLERS = [
+  ...COMMAND_HANDLERS_AGENT,
+  ...COMMAND_HANDLERS_BOND,
+  ...COMMAND_HANDLERS_CLEARING,
+  ...COMMAND_HANDLERS_COMPLIANCE,
+  ...COMMAND_HANDLERS_CONTROL_LIST,
+  ...COMMAND_HANDLERS_EQUITY,
+  ...COMMAND_HANDLERS_FREEZE,
+  ...COMMAND_HANDLERS_HOLD,
+  ...COMMAND_HANDLERS_IDENTITY,
+  ...COMMAND_HANDLERS_ISSUE,
+  ...COMMAND_HANDLERS_KYC,
+  ...COMMAND_HANDLERS_LOCK,
+  ...COMMAND_HANDLERS_MOCK,
+  ...COMMAND_HANDLERS_NETWORK,
+  ...COMMAND_HANDLERS_PAUSE,
+  ...COMMAND_HANDLERS_PROTECTED,
+  ...COMMAND_HANDLERS_RBAC,
+  ...COMMAND_HANDLERS_RECOVERY,
+  ...COMMAND_HANDLERS_REDEEM,
+  ...COMMAND_HANDLERS_RESOLVER,
+  ...COMMAND_HANDLERS_METADATA,
+  ...COMMAND_HANDLERS_SNAPSHOT,
+  ...COMMAND_HANDLERS_SUPPLY,
+  ...COMMAND_HANDLERS_TRANSFER,
+  ...COMMAND_HANDLERS_BALANCE,
+];
+
+export const QUERY_HANDLERS = [
+  ...QUERY_HANDLERS_BOND,
+  ...QUERY_HANDLERS_CLEARING,
+  ...QUERY_HANDLERS_COMPLIANCE,
+  ...QUERY_HANDLERS_CONTROL_LIST,
+  ...QUERY_HANDLERS_EQUITY,
+  ...QUERY_HANDLERS_FREEZE,
+  ...QUERY_HANDLERS_HOLD,
+  ...QUERY_HANDLERS_IDENTITY,
+  ...QUERY_HANDLERS_KYC,
+  ...QUERY_HANDLERS_LOCK,
+  ...QUERY_HANDLERS_MOCK,
+  ...QUERY_HANDLERS_PAUSE,
+  ...QUERY_HANDLERS_RBAC,
+  ...QUERY_HANDLERS_RECOVERY,
+  ...QUERY_HANDLERS_REDEEM,
+  ...QUERY_HANDLERS_RESOLVER,
+  ...QUERY_HANDLERS_SUPPLY,
+  ...QUERY_HANDLERS_PROTECTED,
+  ...QUERY_HANDLERS_SECURITY_DETAILS,
+  ...QUERY_HANDLERS_BALANCE,
+  ...QUERY_HANDLERS_TRANSFER,
+  ...QUERY_HANDLERS_ACCOUNT,
+  ...QUERY_HANDLERS_OPERATOR,
+];
+
+export const TRANSACTION_HANDLER = [
+  {
+    token: TOKENS.TRANSACTION_HANDLER,
+    useClass: RPCTransactionAdapter,
   },
-  rpcNode: {
-    name: 'default',
-    baseUrl: 'https://testnet.hashio.io/api',
+  {
+    token: TOKENS.TRANSACTION_HANDLER,
+    useClass: HederaWalletConnectTransactionAdapter,
   },
-};
-
-// Network default props
-container.register<NetworkProps>('NetworkProps', {
-  useValue: defaultNetworkProps,
-});
-
-// Wallet events
-container.register<typeof WalletEvents>('WalletEvents', {
-  useValue: WalletEvents,
-});
-
-// SDK Logs
-container.register<typeof SDK>('SDK', {
-  useValue: SDK,
-});
-
-@registry([...COMMAND_HANDLERS, ...QUERY_HANDLERS, ...TRANSACTION_HANDLER])
-export default class Injectable {
-  static readonly TOKENS = TOKENS;
-
-  private static currentTransactionHandler: TransactionAdapter;
-
-  static resolve<T = unknown>(cls: InjectionToken<T>): T {
-    return container.resolve(cls);
-  }
-
-  static lazyResolve<T = unknown>(cls: Constructor<T>): T {
-    return container.resolve(delay(() => cls));
-  }
-
-  static getQueryHandlers(): QueryHandlerType[] {
-    return container.resolveAll<QueryHandlerType>(TOKENS.QUERY_HANDLER);
-  }
-
-  static getCommandHandlers(): CommandHandlerType[] {
-    return container.resolveAll<CommandHandlerType>(TOKENS.COMMAND_HANDLER);
-  }
-
-  static register<T = unknown>(
-    token: InjectionToken<T>,
-    value: ValueProvider<T>,
-  ): DependencyContainer {
-    return container.register(token, value);
-  }
-
-  static registerCommandHandler<T = unknown>(
-    cls: ValueProvider<T>,
-  ): DependencyContainer {
-    return container.register(TOKENS.COMMAND_HANDLER, cls);
-  }
-
-  static registerTransactionHandler<T extends TransactionAdapter>(
-    cls: T,
-  ): boolean {
-    if (this.currentTransactionHandler) this.currentTransactionHandler.stop();
-    this.currentTransactionHandler = cls;
-    return true;
-  }
-
-  static resolveTransactionHandler(): TransactionAdapter {
-    if (!this.currentTransactionHandler) {
-      throw new RuntimeError('No Transaction Handler registered!');
-    } else {
-      return this.currentTransactionHandler;
-    }
-  }
-
-  static registerTransactionAdapterInstances(): TransactionAdapter[] {
-    const adapters: TransactionAdapter[] = [];
-    if (this.isWeb()) {
-      adapters.push(
-        Injectable.resolve(RPCTransactionAdapter),
-        Injectable.resolve(HederaWalletConnectTransactionAdapter),
-      );
-    }
-    adapters.push(
-      Injectable.resolve(DFNSTransactionAdapter),
-      Injectable.resolve(FireblocksTransactionAdapter),
-      Injectable.resolve(AWSKMSTransactionAdapter),
-    );
-    return adapters;
-  }
-
-  static isWeb(): boolean {
-    return !!global.window;
-  }
-}
+  {
+    token: TOKENS.TRANSACTION_HANDLER,
+    useClass: DFNSTransactionAdapter,
+  },
+  {
+    token: TOKENS.TRANSACTION_HANDLER,
+    useClass: FireblocksTransactionAdapter,
+  },
+  {
+    token: TOKENS.TRANSACTION_HANDLER,
+    useClass: AWSKMSTransactionAdapter,
+  },
+];
