624dc76000e39c86041d77b8c5344df0b995f4d9
Miguel_LZPF <miguel.carpena@io.builders>
2025-08-04 14:36:52 +0200

    feat: implement ERC1410 read functionality with resolver keys and static function selectors
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- contracts/contracts/layer_1/ERC1400/ERC1410/ERC1410Read.sol
+++ contracts/contracts/layer_1/ERC1400/ERC1410/ERC1410Read.sol
@@ -49,8 +49,8 @@
       "Contribution" shall mean any work of authorship, including
       the original version of the Work and any modifications or additions
       to that Work or Derivative Works thereof, that is intentionally
-      submitted to Licensor for inclusion in the Work by the copyright owner
-      or by an individual or Legal Entity authorized to submit on behalf of
+      submitted for inclusion in the Work by the copyright owner or
+      by an individual or Legal Entity authorized to submit on behalf of
       the copyright owner. For the purposes of this definition, "submitted"
       means any form of electronic, verbal, or written communication sent
       to the Licensor or its representatives, including but not limited to
@@ -203,103 +203,117 @@
 
 */
 
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
-// SPDX-License-Identifier: BSD-3-Clause-Attribution
 
-// solhint-disable max-line-length
+import {IERC1410Read} from '../../interfaces/ERC1400/IERC1410Read.sol';
+import {Common} from '../../common/Common.sol';
+import {
+    ERC1410StorageWrapper
+} from '../../../layer_0/ERC1400/ERC1410/ERC1410StorageWrapper.sol';
 
-// keccak256('security.token.standard.accesscontrol.resolverKey');
-bytes32 constant _ACCESS_CONTROL_RESOLVER_KEY = 0x011768a41cb4fe76a26f444eec15d81a0d84e919a36336d72c6539cf41c0fcf6;
+/**
+ * @title ERC1410Read
+ * @dev Facet containing all read-only operations for ERC1410 functionality
+ * @notice This facet handles balance queries, partition queries, operator queries, and validation queries
+ */
+abstract contract ERC1410Read is IERC1410Read, Common, ERC1410StorageWrapper {
+    function balanceOf(address _tokenHolder) external view returns (uint256) {
+        return _balanceOfAdjusted(_tokenHolder);
+    }
 
-// keccak256('security.token.standard.controllist.resolverKey');
-bytes32 constant _CONTROL_LIST_RESOLVER_KEY = 0xfbb1491bfcecd95f79409bd5a4b69a4ba1e5573573372f5d2d66c11e3016414c;
+    function balanceOfAt(
+        address _tokenHolder,
+        uint256 _timestamp
+    ) external view returns (uint256) {
+        return _balanceOfAdjustedAt(_tokenHolder, _timestamp);
+    }
 
-// keccak256('security.token.standard.pause.resolverKey');
-bytes32 constant _PAUSE_RESOLVER_KEY = 0x9429fd9ef38f89f41bd9ec33fd5c94b287ed1c27a98938da43835ac761b2f92c;
+    function balanceOfByPartition(
+        bytes32 _partition,
+        address _tokenHolder
+    ) external view returns (uint256) {
+        return _balanceOfByPartitionAdjusted(_partition, _tokenHolder);
+    }
 
-// keccak256('security.token.standard.cap.resolverKey');
-bytes32 constant _CAP_RESOLVER_KEY = 0xfb3f8aac36661b5540c571d821c80dc9db7ede5ca2a4204ee562b3356f0c026b;
+    function totalSupply() external view returns (uint256) {
+        return _totalSupplyAdjusted();
+    }
 
-// keccak256('security.token.standard.erc20.resolverKey');
-bytes32 constant _ERC20_RESOLVER_KEY = 0x064c883089ba1a596d9146c7aaa73c19ef8825f374c67a9538787c3d12e68dc5;
+    function totalSupplyByPartition(
+        bytes32 _partition
+    ) external view returns (uint256) {
+        return _totalSupplyByPartitionAdjusted(_partition);
+    }
 
-// keccak256('security.token.standard.erc1594.resolverKey');
-bytes32 constant _ERC1594_RESOLVER_KEY = 0xcb70773e8163595d8bd906e277adeb3935976ad802ee8c29face3dfb0263291f;
+    function partitionsOf(
+        address _tokenHolder
+    ) external view returns (bytes32[] memory) {
+        return _partitionsOf(_tokenHolder);
+    }
 
-// keccak256('security.token.standard.erc1643.resolverKey');
-bytes32 constant _ERC1643_RESOLVER_KEY = 0x24543637956a3076689f171d3932b10f22d40f3785d53acebb340f37bed01625;
+    function isMultiPartition() external view returns (bool) {
+        return _isMultiPartition();
+    }
 
-// keccak256('security.token.standard.erc1410.read.resolverKey');
-bytes32 constant _ERC1410_READ_RESOLVER_KEY = 0x4b82da829715756e6816dd3aae969075a28f427c46cddcfda95a83e6c0eb8ad1;
+    function canTransferByPartition(
+        address _from,
+        address _to,
+        bytes32 _partition,
+        uint256 _value,
+        bytes calldata _data,
+        bytes calldata _operatorData
+    ) external view returns (bool, bytes1, bytes32) {
+        (
+            bool status,
+            bytes1 statusCode,
+            bytes32 reason,
 
-// keccak256('security.token.standard.erc1410.transfer.resolverKey');
-bytes32 constant _ERC1410_TRANSFER_RESOLVER_KEY = 0x89fcc808d9a9a4ed7e0e35ec745380066c3f670bd1bda59e8221a0e891635fea;
+        ) = _isAbleToTransferFromByPartition(
+                _from,
+                _to,
+                _partition,
+                _value,
+                _data,
+                _operatorData
+            );
+        return (status, statusCode, reason);
+    }
 
-// keccak256('security.token.standard.erc1410.management.resolverKey');
-bytes32 constant _ERC1410_MANAGEMENT_RESOLVER_KEY = 0xcb72e20ff93a1627a24ce147e6017a845008f80f11534d7775f72ce43af82143;
+    function canRedeemByPartition(
+        address _from,
+        bytes32 _partition,
+        uint256 _value,
+        bytes calldata _data,
+        bytes calldata _operatorData
+    ) external view override returns (bool, bytes1, bytes32) {
+        (
+            bool status,
+            bytes1 code,
+            bytes32 reason,
 
-// keccak256('security.token.standard.erc1644.resolverKey');
-bytes32 constant _ERC1644_RESOLVER_KEY = 0xf1da2ed271d62ba0b6597874c96fb6ed7d929e5ec679f4ad8c2c516c72f6736d;
+        ) = _isAbleToRedeemFromByPartition(
+                _from,
+                _partition,
+                _value,
+                _data,
+                _operatorData
+            );
+        return (status, code, reason);
+    }
 
-// keccak256('security.token.standard.snapshots.resolverKey');
-bytes32 constant _SNAPSHOTS_RESOLVER_KEY = 0x9a3fc46d83536ef6b87eb4fec37302bfd1a7c18e81ea2da853b911b44cf5b0cf;
+    function isOperator(
+        address _operator,
+        address _tokenHolder
+    ) public view returns (bool) {
+        return _isOperator(_operator, _tokenHolder);
+    }
 
-// keccak256("security.token.standard.resolver.proxy.resolverKey")
-bytes32 constant _RESOLVER_PROXY_RESOLVER_KEY = 0x6fe19cad2a96b3f5852be16d059cc4c233139891fc04dc506c03d297d5f12c1e;
-
-// keccak256("security.token.standard.diamond.loupe.resolverKey")
-bytes32 constant _DIAMOND_LOUPE_RESOLVER_KEY = 0x086a1dd0b9bfa39267d1de30445a8edeb3a1f50c8a0a82c91f9dee3608e83567;
-
-// keccak256("security.token.standard.diamond.cut.resolverKey")
-bytes32 constant _DIAMOND_CUT_RESOLVER_KEY = 0xb66fc45b2670ed2c4ce03061121e6c8e53bce06e161f95afad8e57671b64fca8;
-
-// keccak256("security.token.standard.diamond.resolverKey")
-bytes32 constant _DIAMOND_RESOLVER_KEY = 0x1b5212ea37fb29e99afa2812a5d7d7e662a477424d3de1a18cc3871a2ee94d78;
-
-// keccak256("security.token.standard.corporateActions.resolverKey")
-bytes32 constant _CORPORATE_ACTIONS_RESOLVER_KEY = 0x3cc74200ccfb5d585a6d170f8824979dbf1b592e0a41eef41cf6d86cf4882077;
-
-// keccak256("security.token.standard.lock.resolverKey")
-bytes32 constant _LOCK_RESOLVER_KEY = 0xf1364345b3db5ebe5808f2d2d2aaecb9cdb4fddacad1534033060ebc886fc1e9;
-
-// keccak256("security.token.standard.protected.partitions.resolverKey")
-bytes32 constant _PROTECTED_PARTITIONS_RESOLVER_KEY = 0x6d65d2938c05a4d952aff0845c1baa5bea04d4544db74f8b3b26004d1d58d58f;
-
-// keccak256("security.token.standard.hold.resolverKey")
-bytes32 constant _HOLD_RESOLVER_KEY = 0x49c539aaa7273888cd36649dc1b9a58a0bc7f54b639f84f2a7074d99b0754f6d;
-
-// keccak256("security.token.standard.ssi.management.resolverKey")
-bytes32 constant _SSI_MANAGEMENT_RESOLVER_KEY = 0x46df6aaf3742e0cbad136a74fb679b686e087dcc3a3d92d1c4ce2f3ef1b508a0;
-
-// keccak256("security.token.standard.kyc.resolverKey")
-bytes32 constant _KYC_RESOLVER_KEY = 0xf516a0f6b4726244ae916c590cd26c2b593d7d448e46e43714fb9f9435c46e32;
-
-// keccak256("security.token.standard.clearing.transfer.resolverKey")
-bytes32 constant _CLEARING_TRANSFER_RESOLVER_KEY = 0x7399d03db62430bec60ca2c3eacf98b1b7e2253f17593ef7a226d759442e0928;
-
-// keccak256("security.token.standard.clearing.redeem.resolverKey")
-bytes32 constant _CLEARING_REDEEM_RESOLVER_KEY = 0xb341e7aa749da43976c189209de51ccdf838af9f964cd27340b914d5b2aeba97;
-
-// keccak256("security.token.standard.clearing.holdCreation.resolverKey")
-bytes32 constant _CLEARING_HOLDCREATION_RESOLVER_KEY = 0x44f99a141c434fac20d69e7511932ee344d5b37b61851976c83a5df4ca468152;
-
-// keccak256("security.token.standard.clearing.read.resolverKey")
-bytes32 constant _CLEARING_READ_RESOLVER_KEY = 0xebb2e29bdf4edaf4ca66a3f9b7735087f9d0474d56d856e53c94ef00596c0b1e;
-
-// keccak256("security.token.standard.clearing.actions.resolverKey")
-bytes32 constant _CLEARING_ACTIONS_RESOLVER_KEY = 0x5472dfc5c92ad7a8651518ea7d3854d3b6494e5bcaa19f91cd61bf93bf6f2a74;
-
-// keccak256("security.token.standard.pause.management.resolverKey")
-bytes32 constant _PAUSE_MANAGEMENT_RESOLVER_KEY = 0xadd2e196c17b4f607e327e46341eedbbbc3dce86ac90ceb3e7244b0a5f8590ac;
-
-// keccak256("security.token.standard.controllist.management.resolverKey")
-bytes32 constant _CONTROL_LIST_MANAGEMENT_RESOLVER_KEY = 0xb28d59e89fa116cebe06d8de737191b637a49d95f7d8d947d47ac000463e7c71;
-
-// keccak256("security.token.standard.kyc.management.resolverKey")
-bytes32 constant _KYC_MANAGEMENT_RESOLVER_KEY = 0x8676785f4d841823214e8ee8c497b3336a210be7559f5571c590249f6203e821;
-
-// keccak256('security.token.standard.erc3643.resolverKey');
-bytes32 constant _ERC3643_RESOLVER_KEY = 0x9670cee833e6302b5aeea3ae5875f87bd0af95a1fc660ca37cf38b1aa887fecb;
-
-// keccak256('security.token.standard.freeze.resolverKey');
-bytes32 constant _FREEZE_RESOLVER_KEY = 0x49f765e7155d979a148049c2a0ebed5e028b11799061897a255f99314f0bd3f1;
+    function isOperatorForPartition(
+        bytes32 _partition,
+        address _operator,
+        address _tokenHolder
+    ) public view returns (bool) {
+        return _isOperatorForPartition(_partition, _operator, _tokenHolder);
+    }
+}
