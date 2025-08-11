1fbdaab439e113edc6da67af72e848814c5a3504
Miguel_LZPF <miguel.carpena@io.builders>
2025-08-11 11:18:00 +0200

    chore: test linter
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- apps/ats/web/src/components/CancelButton.tsx
+++ apps/ats/web/src/components/CancelButton.tsx
@@ -203,15 +203,15 @@
 
 */
 
-import { Button } from "io-bricks-ui";
-import type { ButtonProps } from "io-bricks-ui";
-import { useTranslation } from "react-i18next";
-import { RouterManager } from "../router/RouterManager";
-import { RouteName } from "../router/RouteName";
-import { Link as RouterLink } from "react-router-dom";
+import { Button } from 'io-bricks-ui';
+import type { ButtonProps } from 'io-bricks-ui';
+import { useTranslation } from 'react-i18next';
+import { RouterManager } from '../router/RouterManager';
+import { RouteName } from '../router/RouteName';
+import { Link as RouterLink } from 'react-router-dom';
 
 export const CancelButton = (props: ButtonProps) => {
-  const { t: tGlobals } = useTranslation("globals");
+  const { t: tGlobals } = useTranslation('globals');
 
   return (
     <Button
@@ -222,7 +222,7 @@ export const CancelButton = (props: ButtonProps) => {
       variant="secondary"
       {...props}
     >
-      {tGlobals("cancel")}
+      {tGlobals('cancel')}
     </Button>
   );
 };
