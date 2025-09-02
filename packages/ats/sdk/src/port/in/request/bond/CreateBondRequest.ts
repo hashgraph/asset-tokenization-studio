2d9142063cfa5d0b43020d7d12acf8bfdd51f283
jaime-iobermudez <jaime.bermudez@io.builders>
2025-09-02 13:22:43 +0200

    refactor: review params (#614)
    Signed-off-by: Alberto Molina <alberto@io.builders>
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    Signed-off-by: Mario Francia <mario@io.builders>
    Signed-off-by: jaime-iobermudez <jaime.bermudez@io.builders>
    Signed-off-by: Axel Loupias <108282711+Axel-IoBuilders@users.noreply.github.com>
    Signed-off-by: Roberto Delgado <roberto@io.builders>
    Signed-off-by: Axel Loupias <axel@io.builders>
    Co-authored-by: Alberto Molina <alberto@io.builders>
    Co-authored-by: Miguel_LZPF <miguel.carpena@io.builders>
    Co-authored-by: Mario Francia <mario@io.builders>
    Co-authored-by: Roberto Delgado <roberto@io.builders>
    Co-authored-by: Axel Loupias <axel@io.builders>

--- packages/ats/sdk/src/port/in/request/bond/CreateBondRequest.ts
+++ packages/ats/sdk/src/port/in/request/bond/CreateBondRequest.ts
@@ -231,13 +231,13 @@ export default class CreateBondRequest extends ValidatedRequest<CreateBondReques
   internalKycActivated: boolean;
 
   @OptionalField()
-  externalPauses?: string[];
+  externalPausesIds?: string[];
 
   @OptionalField()
-  externalControlLists?: string[];
+  externalControlListsIds?: string[];
 
   @OptionalField()
-  externalKycLists?: string[];
+  externalKycListsIds?: string[];
 
   @OptionalField()
   diamondOwnerAccount?: string;
@@ -276,9 +276,9 @@ export default class CreateBondRequest extends ValidatedRequest<CreateBondReques
     isMultiPartition,
     clearingActive,
     internalKycActivated,
-    externalPauses,
-    externalControlLists,
-    externalKycLists,
+    externalPausesIds,
+    externalControlListsIds,
+    externalKycListsIds,
     diamondOwnerAccount,
     currency,
     numberOfUnits,
@@ -309,9 +309,9 @@ export default class CreateBondRequest extends ValidatedRequest<CreateBondReques
     isMultiPartition: boolean;
     clearingActive: boolean;
     internalKycActivated: boolean;
-    externalPauses?: string[];
-    externalControlLists?: string[];
-    externalKycLists?: string[];
+    externalPausesIds?: string[];
+    externalControlListsIds?: string[];
+    externalKycListsIds?: string[];
     diamondOwnerAccount?: string;
     currency: string;
     numberOfUnits: string;
@@ -381,24 +381,24 @@ export default class CreateBondRequest extends ValidatedRequest<CreateBondReques
         return Factory.checkRegulationSubType(val, this.regulationType);
       },
       configId: FormatValidation.checkBytes32Format(),
-      externalPauses: (val) => {
+      externalPausesIds: (val) => {
         return FormatValidation.checkHederaIdOrEvmAddressArray(
           val ?? [],
-          'externalPauses',
+          'externalPausesIds',
           true,
         );
       },
-      externalControlLists: (val) => {
+      externalControlListsIds: (val) => {
         return FormatValidation.checkHederaIdOrEvmAddressArray(
           val ?? [],
-          'externalControlLists',
+          'externalControlListsIds',
           true,
         );
       },
-      externalKycLists: (val) => {
+      externalKycListsIds: (val) => {
         return FormatValidation.checkHederaIdOrEvmAddressArray(
           val ?? [],
-          'externalKycLists',
+          'externalKycListsIds',
           true,
         );
       },
@@ -419,9 +419,9 @@ export default class CreateBondRequest extends ValidatedRequest<CreateBondReques
     this.clearingActive = clearingActive;
     this.internalKycActivated = internalKycActivated;
     this.diamondOwnerAccount = diamondOwnerAccount;
-    this.externalPauses = externalPauses;
-    this.externalControlLists = externalControlLists;
-    this.externalKycLists = externalKycLists;
+    this.externalPausesIds = externalPausesIds;
+    this.externalControlListsIds = externalControlListsIds;
+    this.externalKycListsIds = externalKycListsIds;
     this.currency = currency;
     this.numberOfUnits = numberOfUnits;
     this.nominalValue = nominalValue;
