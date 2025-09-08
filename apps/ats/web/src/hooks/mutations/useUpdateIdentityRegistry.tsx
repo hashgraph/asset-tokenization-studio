aa8933b0dac915d45af1503c40d7f4bedf2afa42
Manu Fernández <manuel@io.builders>
2025-09-08 09:14:34 +0200

    Feat: [BBND-1078] T-Rex missing features (#628)
    Signed-off-by: Alberto Molina <alberto@io.builders>
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    Signed-off-by: Mario Francia <mario@io.builders>
    Signed-off-by: Axel Loupias <108282711+Axel-IoBuilders@users.noreply.github.com>
    Signed-off-by: Roberto Delgado <roberto@io.builders>
    Signed-off-by: Manu Fernández <manuel@io.builders>
    Signed-off-by: Axel Loupias <axel@io.builders>
    Co-authored-by: Alberto Molina <alberto@io.builders>
    Co-authored-by: Miguel_LZPF <miguel.carpena@io.builders>
    Co-authored-by: Mario Francia <mario@io.builders>
    Co-authored-by: Roberto Delgado <roberto@io.builders>
    Co-authored-by: Axel Loupias <axel@io.builders>

--- apps/ats/web/src/hooks/mutations/useUpdateIdentityRegistry.tsx
+++ apps/ats/web/src/hooks/mutations/useUpdateIdentityRegistry.tsx
@@ -203,189 +203,53 @@
 
 */
 
-import {
-  DefinitionList,
-  DefinitionListProps,
-  ClipboardButton,
-  Text,
-} from 'io-bricks-ui';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import SDKService from '../../services/SDKService';
+import { SetIdentityRegistryRequest } from '@hashgraph/asset-tokenization-sdk';
+import { useToast } from 'io-bricks-ui';
 import { useTranslation } from 'react-i18next';
-import {
-  formatDate,
-  formatNumberLocale,
-  toNumber,
-} from '../../../utils/format';
-import { useSecurityStore } from '../../../store/securityStore';
-import { useParams } from 'react-router-dom';
-import { Flex } from '@chakra-ui/react';
-import {
-  BondDetailsViewModel,
-  ComplianceRequest,
-  EquityDetailsViewModel,
-  IdentityRegistryRequest,
-} from '@hashgraph/asset-tokenization-sdk';
-import { useMemo } from 'react';
-import { MaturityDateItem } from './MadurityDateItem';
-import { DATE_TIME_FORMAT } from '../../../utils/constants';
-import { useGetCompliance } from '../../../hooks/queries/useCompliance';
-import { useGetIdentityRegistry } from '../../../hooks/queries/useIdentityRegistry';
-import { ComplianceItem } from './ComplianceItem';
-import { IdentityRegistryItem } from './IdentityRegistryItem';
+import { GET_IDENTITY_REGISTRY } from '../queries/useIdentityRegistry';
 
-interface SecurityDetailsExtendedProps
-  extends Omit<DefinitionListProps, 'items'> {
-  bondDetailsResponse?: BondDetailsViewModel;
-  equityDetailsResponse?: EquityDetailsViewModel;
-  isLoadingSecurityDetails: boolean;
-  isFetchingSecurityDetails: boolean;
-}
+export const useUpdateIdentityRegistry = () => {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+  const { t } = useTranslation('security', {
+    keyPrefix: 'details.bond.updateIdentityRegistry.messages',
+  });
 
-export const SecurityDetailsExtended = ({
-  bondDetailsResponse,
-  equityDetailsResponse,
-  isLoadingSecurityDetails,
-  isFetchingSecurityDetails,
-  ...props
-}: SecurityDetailsExtendedProps) => {
-  const { t: tProperties } = useTranslation('properties');
-  const { details } = useSecurityStore();
-  const { id } = useParams();
-
-  const { data: compliance } = useGetCompliance(
-    new ComplianceRequest({
-      securityId: id!,
-    }),
+  return useMutation(
+    (req: SetIdentityRegistryRequest) => SDKService.updateIdentityRegistry(req),
     {
-      enabled: !!id,
+      onSuccess(data, variables) {
+        queryClient.invalidateQueries({
+          queryKey: [GET_IDENTITY_REGISTRY(variables.securityId)],
+        });
+
+        console.log('SDK message --> Update identity registry success: ', data);
+
+        if (!data) {
+          return;
+        }
+
+        toast.show({
+          duration: 3000,
+          title: t('success'),
+          description: t('updateIdentityRegistrySuccessful'),
+          variant: 'subtle',
+          status: 'success',
+        });
+      },
+      onError: (error) => {
+        console.log('SDK message --> Update identity registry error: ', error);
+
+        toast.show({
+          duration: 3000,
+          title: t('error'),
+          description: t('updateIdentityRegistryFailed'),
+          variant: 'subtle',
+          status: 'error',
+        });
+      },
     },
-  );
-
-  const { data: identityRegistry } = useGetIdentityRegistry(
-    new IdentityRegistryRequest({
-      securityId: id!,
-    }),
-    {
-      enabled: !!id,
-    },
-  );
-
-  const nominalValue = useMemo(() => {
-    return toNumber(
-      equityDetailsResponse?.nominalValue || bondDetailsResponse?.nominalValue,
-      2,
-    );
-  }, [equityDetailsResponse, bondDetailsResponse]);
-
-  const listItems = useMemo(() => {
-    const items = [
-      {
-        title: tProperties('name'),
-        description: details?.name ?? '',
-      },
-      {
-        title: tProperties('symbol'),
-        description: details?.symbol ?? '',
-      },
-      {
-        title: tProperties('decimal'),
-        description: details?.decimals ?? '',
-      },
-      {
-        title: tProperties('isin'),
-        description: details?.isin ?? '',
-      },
-      {
-        title: tProperties('evmAddress'),
-        description: details?.evmDiamondAddress ?? '',
-        canCopy: true,
-      },
-      {
-        title: tProperties('id'),
-        description: (
-          <Flex w="full" align="center">
-            <Text textStyle="ElementsRegularSM">{id}</Text>
-            <ClipboardButton value={id!} />
-            <Text textStyle="ElementsSemiboldXS" ml={4}>
-              {tProperties('copyId')}
-            </Text>
-          </Flex>
-        ),
-      },
-      {
-        title: tProperties('currency'),
-        description: 'USD',
-      }, // TODO: - format from ASCII when more currencies are available
-      {
-        title: tProperties('nominalValue'),
-        description: formatNumberLocale(nominalValue, 2),
-      },
-      {
-        title: tProperties('maxSupply'),
-        description: `${details?.maxSupply} ${details?.symbol}`,
-      },
-      {
-        title: tProperties('totalSupply'),
-        description: `${details?.totalSupply} ${details?.symbol}`,
-      },
-      {
-        title: tProperties('pendingToBeMinted'),
-        description: `${
-          toNumber(details?.maxSupply) - toNumber(details?.totalSupply)
-        } ${details?.symbol}`,
-      },
-    ];
-
-    if (compliance !== undefined && id) {
-      items.push({
-        title: tProperties('compliance'),
-        description: <ComplianceItem securityId={id} />,
-      });
-    }
-
-    if (identityRegistry !== undefined && id) {
-      items.push({
-        title: tProperties('identityRegistry'),
-        description: <IdentityRegistryItem securityId={id} />,
-      });
-    }
-
-    const isBond = details?.type === 'BOND';
-
-    if (isBond && bondDetailsResponse?.startingDate) {
-      items.push({
-        title: tProperties('startingDate'),
-        description: formatDate(
-          bondDetailsResponse.startingDate,
-          DATE_TIME_FORMAT,
-        ),
-      });
-    }
-
-    if (isBond && bondDetailsResponse?.maturityDate && id) {
-      items.push({
-        title: tProperties('maturityDate'),
-        description: <MaturityDateItem securityId={id} />,
-      });
-    }
-
-    return items;
-  }, [
-    details,
-    id,
-    nominalValue,
-    tProperties,
-    bondDetailsResponse,
-    compliance,
-    identityRegistry,
-  ]);
-
-  return (
-    <DefinitionList
-      data-testid="security-details"
-      isLoading={isLoadingSecurityDetails || isFetchingSecurityDetails}
-      items={listItems}
-      title="Details"
-      {...props}
-    />
   );
 };
