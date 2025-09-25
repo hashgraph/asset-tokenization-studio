68c85460795b15bfa0fd2892f0485cf5ca1bbbf4
Mario Francia <mariofranciarius@gmail.com>
2025-09-25 12:06:25 +0200

    feat: mass payout (#672)
    Signed-off-by: Mario Francia <mario@io.builders>
    Signed-off-by: Manu Fernández <manuel@io.builders>
    Co-authored-by: Manu Fernández <manuel@io.builders>

---
+++ apps/mass-payout/frontend/src/views/Assets/AssetDistributions/AssetDistributions.types.ts
@@ -0,0 +1,89 @@
+import { Control } from 'react-hook-form';
+import { NavigateFunction } from 'react-router-dom';
+import { ColumnDef } from '@tanstack/react-table';
+import { TFunction } from 'i18next';
+import { UseTableReturn } from '@/hooks/useTable';
+import { Asset } from '@/services/AssetService';
+
+export interface AssetDistributionData {
+  id: string;
+  asset: Asset & {
+    distributedAmount?: number;
+  };
+  type: string;
+  corporateActionID?: string;
+  concept?: string;
+  executionDate?: string;
+  status: string;
+  trigger?: string;
+  actions?: string;
+  holdersNumber?: string;
+  createdAt: string;
+  updatedAt: string;
+  amount?: string;
+  amountType?: string;
+  subtype?: string;
+  nextExecutionTime?: string;
+  recurrency?: string;
+}
+
+export type AssetDistributionColumn = ColumnDef<AssetDistributionData, any>;
+
+export type DistributionFilterType = 'upcoming' | 'ongoing' | 'completed';
+
+export interface AssetDistributionsProps {
+  assetId: string;
+  isPaused?: boolean;
+  onPauseUnpause?: () => void;
+  onImportCorporateActions?: () => void;
+  handleNewDistribution?: () => void;
+  isImportingCorporateActions?: boolean;
+}
+
+export interface AssetDistributionsFormValues {
+  search: string;
+  distributionType: string;
+}
+
+export interface StatusFilterMap {
+  upcoming: (item: AssetDistributionData) => boolean;
+  ongoing: (item: AssetDistributionData) => boolean;
+  completed: (item: AssetDistributionData) => boolean;
+}
+
+export interface TabTitleMap {
+  upcoming: string;
+  ongoing: string;
+  completed: string;
+}
+
+export type ColumnsByTab = Record<
+  DistributionFilterType,
+  AssetDistributionColumn[]
+>;
+
+export interface DistributionTabContent {
+  title: string;
+  columns: AssetDistributionColumn[];
+  filteredDistributions: AssetDistributionData[];
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface FilteredDistributionsResult {
+  filteredDistributions: AssetDistributionData[];
+  totalFilteredElements: number;
+}
+
+export interface TabContentProps {
+  filterType: 'upcoming' | 'ongoing' | 'completed';
+  control: Control<AssetDistributionsFormValues>;
+  navigate: NavigateFunction;
+  table: UseTableReturn;
+  t: TFunction;
+  columns: AssetDistributionColumn[];
+  filteredDistributions: AssetDistributionData[];
+  totalFilteredElements: number;
+  isLoading: boolean;
+  id?: string;
+}
