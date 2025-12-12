e97aa1643fc75d974a567c75cededf6bbfd8479f
Miguel Ángel <miguelangel@io.builders>
2025-12-12 10:01:43 +0100

    feat: enhance swagger documentation
    Mass Payout
    
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- apps/mass-payout/backend/src/infrastructure/rest/distribution/distribution.response.ts
+++ apps/mass-payout/backend/src/infrastructure/rest/distribution/distribution.response.ts
@@ -212,24 +212,99 @@ import {
 } from "@domain/model/distribution"
 import { AssetResponse } from "@infrastructure/rest/asset/asset.response"
 import { Logger } from "@nestjs/common"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class DistributionResponse {
   private static readonly logger = new Logger(DistributionResponse.name)
 
+  @ApiProperty({
+    description: "The id of the distribution in the service",
+    example: "32264ce1-76fb-44bb-a8a2-d1a516dcf34d",
+  })
   id: string
+
+  @ApiProperty({
+    type: () => AssetResponse,
+  })
   asset: AssetResponse
+
+  @ApiProperty({
+    description: "The distribution type",
+    example: "PAYOUT | CORPORATE_ACTION",
+  })
   type: string
+
+  @ApiProperty({
+    description: "The ID of the corporate action",
+    example: "23514",
+    required: false,
+  })
   corporateActionID?: string
+
+  @ApiProperty({
+    description: "The execution date",
+    example: "2025-01-15 13:45:30",
+    required: false,
+  })
   executionDate?: Date
+
+  @ApiProperty({
+    description: "The amount of the distribution when the distribution corresponds to a snapshot payment",
+    example: "50",
+    required: false,
+  })
   amount?: string
+
+  @ApiProperty({
+    description: "The amount type when the distribution corresponds to a snapshot payment",
+    example: "FIXED | PERCENTAGE",
+    required: false,
+  })
   amountType?: string
+
+  @ApiProperty({
+    description: "The concept",
+    example: "Settlement",
+    required: false,
+  })
   concept?: string
+
+  @ApiProperty({
+    description: "The payout subtype",
+    example: "IMMEDIATE | ONE_OFF | RECURRING | AUTOMATED",
+    required: false,
+  })
   subtype?: string
+
+  @ApiProperty({
+    description: "The status of the distribution process",
+    example: "SCHEDULED | IN_PROGRESS | FAILED | COMPLETED | CANCELLED",
+  })
   status: string
+
+  @ApiProperty({
+    description: "The holders number the distribution has to pay to",
+    example: "80",
+  })
   holdersNumber: number
+
+  @ApiProperty({
+    description: "The recurrency of the distribution process",
+    example: "HOURLY | DAILY | WEEKLY | MONTHLY",
+    required: false,
+  })
   recurrency?: Recurrency
 
+  @ApiProperty({
+    description: "The distribution creation date in the service",
+    example: "2025-01-15 13:45:30",
+  })
   createdAt: Date
+
+  @ApiProperty({
+    description: "The distribution update date in the service",
+    example: "2025-01-15 13:45:30",
+  })
   updatedAt: Date
 
   static fromDistribution(distribution: Distribution): DistributionResponse {
--- apps/mass-payout/backend/src/infrastructure/rest/page-options.request.ts
+++ apps/mass-payout/backend/src/infrastructure/rest/page-options.request.ts
@@ -205,26 +205,31 @@
 import { PageOptions } from "@domain/model/page"
 import { Type } from "class-transformer"
 import { IsIn, IsInt, IsOptional, IsString, Min } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
 
 export class PageOptionsRequest {
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
+  @ApiProperty({ description: "The page number", example: "1" })
   page: number = 1
 
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
+  @ApiProperty({ description: "The maximum number of items by page", example: "6" })
   limit: number = 10
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ description: "The criterion to order the results", example: "createdAt" })
   orderBy: string = "createdAt"
 
   @IsOptional()
   @IsIn(["ASC", "DESC", "asc", "desc"])
+  @ApiProperty({ description: "Whether the results will be ordered ascending or descending", example: "DESC" })
   order: string = "DESC"
 
   toPageOptions(): PageOptions {
