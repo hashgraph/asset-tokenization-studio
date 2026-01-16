6b83a04cdab161fecd8e470d003e3c93091ba3cd
mamoralesiob <miguelangel@io.builders>
2026-01-16 11:29:10 +0100

    fix: mass payout contracts audit issues fixes and swagger documentation (#777)
    Signed-off-by: mamoralesiob <miguelangel@io.builders>
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- apps/mass-payout/backend/src/infrastructure/rest/distribution/distribution.controller.ts
+++ apps/mass-payout/backend/src/infrastructure/rest/distribution/distribution.controller.ts
@@ -209,11 +209,13 @@ import { PageOptionsRequest } from "@infrastructure/rest/page-options.request"
 import { PageResponse } from "@infrastructure/rest/page.response"
 import { RestController } from "@infrastructure/rest/rest.decorator"
 import { Get, HttpCode, HttpStatus, Param, Patch, Query } from "@nestjs/common"
+import { ApiOperation, ApiResponse, ApiTags, ApiParam } from "@nestjs/swagger"
 import { HolderResponse } from "@infrastructure/rest/responses/holder.response"
 import { GetDistributionUseCase } from "@application/use-cases/get-distribution.use-case"
 import { CancelDistributionUseCase } from "@application/use-cases/cancel-distribution.use-case"
 import { RetryFailedHoldersUseCase } from "@application/use-cases/retry-failed-holders.use-case"
 
+@ApiTags("Distributions")
 @RestController("distributions")
 export class DistributionController {
   constructor(
@@ -224,6 +226,12 @@ export class DistributionController {
     private readonly retryFailedHoldersUseCase: RetryFailedHoldersUseCase,
   ) {}
 
+  @ApiOperation({ summary: "Get all distributions managed by the Scheduler Payment Distribution Service." })
+  @ApiResponse({ status: 500, description: "Internal server error" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  @ApiResponse({ status: 404, description: "Not Found" })
+  @ApiResponse({ status: 200, type: PageResponse<DistributionResponse> })
   @Get()
   async getDistributions(@Query() pageOptionsRequest: PageOptionsRequest): Promise<PageResponse<DistributionResponse>> {
     const pageOptions = pageOptionsRequest.toPageOptions()
@@ -231,12 +239,36 @@ export class DistributionController {
     return PageResponse.fromPage(distributions, DistributionResponse.fromDistribution)
   }
 
+  @ApiOperation({ summary: "Get a certain distribution managed by the Scheduler Payment Distribution Service." })
+  @ApiParam({
+    name: "distributionId",
+    description: "The ID of the distribution to get.",
+    example: "123456",
+  })
+  @ApiResponse({ status: 500, description: "Internal server error" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  @ApiResponse({ status: 404, description: "Not Found" })
+  @ApiResponse({ status: 200, type: DistributionResponse })
   @Get(":distributionId")
   async getDistribution(@Param("distributionId") distributionId: string): Promise<DistributionResponse> {
     const distribution = await this.getDistributionUseCase.execute(distributionId)
     return DistributionResponse.fromDistribution(distribution)
   }
 
+  @ApiOperation({
+    summary: "Get the holders of a certain distribution managed by the Scheduler Payment Distribution Service.",
+  })
+  @ApiParam({
+    name: "distributionId",
+    description: "The ID of the distribution to get its holders.",
+    example: "123456",
+  })
+  @ApiResponse({ status: 500, description: "Internal server error" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  @ApiResponse({ status: 404, description: "Not Found" })
+  @ApiResponse({ status: 200, type: PageResponse<HolderResponse> })
   @Get(":distributionId/holders")
   async getHolders(
     @Param("distributionId") distributionId: string,
@@ -247,6 +279,20 @@ export class DistributionController {
     return PageResponse.fromPage(holders, HolderResponse.fromHolder)
   }
 
+  @ApiOperation({ summary: "Cancel a distribution." })
+  @ApiParam({
+    name: "distributionId",
+    description: "The ID of the distribution to cancel.",
+    example: "123456",
+  })
+  @ApiResponse({ status: 500, description: "Internal server error" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  @ApiResponse({ status: 404, description: "Not Found" })
+  @ApiResponse({
+    status: 200,
+    description: "The distribution has been successfully cancelled.",
+  })
   @Patch(":distributionId/cancel")
   @HttpCode(HttpStatus.OK)
   async cancelDistribution(@Param("distributionId") distributionId: string): Promise<void> {
@@ -255,6 +301,21 @@ export class DistributionController {
     })
   }
 
+  @ApiOperation({ summary: "Retry a distribution." })
+  @ApiParam({
+    name: "distributionId",
+    description: "The ID of the distribution to cancel.",
+    example: "123456",
+  })
+  @ApiResponse({ status: 500, description: "Internal server error" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  @ApiResponse({ status: 404, description: "Not Found" })
+  @ApiResponse({
+    status: 200,
+    description: "The distribution has been successfully retried.",
+  })
+  @ApiResponse({})
   @Patch(":distributionId/retry")
   @HttpCode(HttpStatus.OK)
   async retryDistribution(@Param("distributionId") distributionId: string): Promise<void> {
--- packages/mass-payout/contracts/contracts/common/LocalContext.sol
+++ packages/mass-payout/contracts/contracts/common/LocalContext.sol
@@ -203,10 +203,10 @@
 
 */
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.22;
 // SPDX-License-Identifier: BSD-3-Clause-Attribution
 
-import { Context } from '@openzeppelin/contracts/utils/Context.sol';
+import { Context } from "@openzeppelin/contracts/utils/Context.sol";
 
 abstract contract LocalContext is Context {
     function _blockTimestamp() internal view virtual returns (uint256 blockTimestamp_) {
