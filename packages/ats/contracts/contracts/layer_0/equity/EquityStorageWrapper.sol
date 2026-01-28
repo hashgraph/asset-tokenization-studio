bc20f958bc0a49b07e6b1cbd21acfc2779e21970
Alberto Molina <alberto@io.builders>
2026-01-28 09:07:12 +0100

    test: unreachable code removed and input parameters type defined (#811)
    Signed-off-by: Alberto Molina <alberto@io.builders>
    Signed-off-by: rbermejo <ruben@io.builders>

--- packages/ats/contracts/contracts/layer_0/equity/EquityStorageWrapper.sol
+++ packages/ats/contracts/contracts/layer_0/equity/EquityStorageWrapper.sol
@@ -64,8 +64,8 @@ abstract contract EquityStorageWrapper is IEquityStorageWrapper, BondStorageWrap
 
         IEquity.Dividend memory newDividend = abi.decode(_data, (IEquity.Dividend));
 
-        _addScheduledCrossOrderedTask(newDividend.recordDate, abi.encode(SNAPSHOT_TASK_TYPE));
-        _addScheduledSnapshot(newDividend.recordDate, abi.encode(_actionId));
+        _addScheduledCrossOrderedTask(newDividend.recordDate, SNAPSHOT_TASK_TYPE);
+        _addScheduledSnapshot(newDividend.recordDate, _actionId);
     }
 
     function _setVoting(
@@ -85,8 +85,8 @@ abstract contract EquityStorageWrapper is IEquityStorageWrapper, BondStorageWrap
 
         IEquity.Voting memory newVoting = abi.decode(_data, (IEquity.Voting));
 
-        _addScheduledCrossOrderedTask(newVoting.recordDate, abi.encode(SNAPSHOT_TASK_TYPE));
-        _addScheduledSnapshot(newVoting.recordDate, abi.encode(_actionId));
+        _addScheduledCrossOrderedTask(newVoting.recordDate, SNAPSHOT_TASK_TYPE);
+        _addScheduledSnapshot(newVoting.recordDate, _actionId);
     }
 
     function _setScheduledBalanceAdjustment(
@@ -112,8 +112,8 @@ abstract contract EquityStorageWrapper is IEquityStorageWrapper, BondStorageWrap
             (IEquity.ScheduledBalanceAdjustment)
         );
 
-        _addScheduledCrossOrderedTask(newBalanceAdjustment.executionDate, abi.encode(BALANCE_ADJUSTMENT_TASK_TYPE));
-        _addScheduledBalanceAdjustment(newBalanceAdjustment.executionDate, abi.encode(_actionId));
+        _addScheduledCrossOrderedTask(newBalanceAdjustment.executionDate, BALANCE_ADJUSTMENT_TASK_TYPE);
+        _addScheduledBalanceAdjustment(newBalanceAdjustment.executionDate, _actionId);
     }
 
     function _getEquityDetails() internal view override returns (IEquity.EquityDetailsData memory equityDetails_) {
