2b405077893feb7afc1e986f948c078b5db2d0ee
adrian <adrian@io.builders>
2026-02-10 14:38:45 +0100

    feat: addKpiData
    Signed-off-by: adrian <adrian@io.builders>

--- packages/ats/sdk/src/app/usecase/command/kpis/addKpiData/AddKpiDataCommandHandler.ts
+++ packages/ats/sdk/src/app/usecase/command/kpis/addKpiData/AddKpiDataCommandHandler.ts
@@ -3,7 +3,7 @@
 import { ICommandHandler } from "@core/command/CommandHandler";
 import { CommandHandler } from "@core/decorator/CommandHandlerDecorator";
 import { AddKpiDataCommand, AddKpiDataCommandResponse } from "./AddKpiDataCommand";
-import TransactionAdapter from "@port/out/TransactionAdapter";
+import TransactionService from "@service/transaction/TransactionService";
 import { lazyInject } from "@core/decorator/LazyInjectDecorator";
 import EvmAddress from "@domain/context/contract/EvmAddress";
 import { AddKpiDataCommandError } from "./error/AddKpiDataCommandError";
@@ -11,15 +11,16 @@ import { AddKpiDataCommandError } from "./error/AddKpiDataCommandError";
 @CommandHandler(AddKpiDataCommand)
 export class AddKpiDataCommandHandler implements ICommandHandler<AddKpiDataCommand, AddKpiDataCommandResponse> {
   constructor(
-    @lazyInject(TransactionAdapter)
-    private readonly transactionAdapter: TransactionAdapter,
+    @lazyInject(TransactionService)
+    private readonly transactionService: TransactionService,
   ) {}
 
   async execute(command: AddKpiDataCommand): Promise<AddKpiDataCommandResponse> {
     try {
       const { securityId, date, value, project } = command;
 
-      const result = await this.transactionAdapter.addKpiData(
+      const transactionAdapter = this.transactionService.getHandler();
+      const result = await transactionAdapter.addKpiData(
         new EvmAddress(securityId),
         date,
         value,
@@ -28,7 +29,8 @@ export class AddKpiDataCommandHandler implements ICommandHandler<AddKpiDataComma
 
       return new AddKpiDataCommandResponse(result.id!);
     } catch (error) {
-      throw new AddKpiDataCommandError(error as string);
+      const errorMessage = error instanceof Error ? error.message : String(error);
+      throw new AddKpiDataCommandError(errorMessage);
     }
   }
 }
