bebacc48f1ef99e9b22095303dc9cd38dd2d5406
Ruben Martinez <ruben.martinez@io.builders>
2026-02-10 11:19:01 +0100

    feat: format validations
    Signed-off-by: Ruben Martinez <ruben.martinez@io.builders>

--- packages/ats/sdk/src/port/in/request/interestRates/SetRateRequest.ts
+++ packages/ats/sdk/src/port/in/request/interestRates/SetRateRequest.ts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import ValidatedRequest from "@core/validation/ValidatedArgs";
+import FormatValidation from "../FormatValidation";
 
 export default class SetRateRequest extends ValidatedRequest<SetRateRequest> {
   securityId: string;
@@ -8,7 +9,11 @@ export default class SetRateRequest extends ValidatedRequest<SetRateRequest> {
   rateDecimals: number;
 
   constructor({ securityId, rate, rateDecimals }: { securityId: string; rate: string; rateDecimals: number }) {
-    super({});
+    super({
+      securityId: FormatValidation.checkHederaIdFormatOrEvmAddress(),
+      rate: FormatValidation.checkNumber({ min: 0 }),
+      rateDecimals: FormatValidation.checkNumber({ min: 0 }),
+    });
     this.securityId = securityId;
     this.rate = rate;
     this.rateDecimals = rateDecimals;
