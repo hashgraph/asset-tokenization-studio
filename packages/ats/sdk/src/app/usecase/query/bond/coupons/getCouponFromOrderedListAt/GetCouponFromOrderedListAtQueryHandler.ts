edfd98ffa74ad3a21158a479f3d9761ede5820bd
Ruben Martinez <ruben.martinez@io.builders>
2026-02-18 11:36:33 +0100

    fix: pr changes
    Signed-off-by: Ruben Martinez <ruben.martinez@io.builders>

--- packages/ats/sdk/src/app/usecase/query/bond/coupons/getCouponFromOrderedListAt/GetCouponFromOrderedListAtQueryHandler.ts
+++ packages/ats/sdk/src/app/usecase/query/bond/coupons/getCouponFromOrderedListAt/GetCouponFromOrderedListAtQueryHandler.ts
@@ -4,28 +4,28 @@ import { lazyInject } from "@core/decorator/LazyInjectDecorator";
 import { QueryHandler } from "@core/decorator/QueryHandlerDecorator";
 import { IQueryHandler } from "@core/query/QueryHandler";
 import { RPCQueryAdapter } from "@port/out/rpc/RPCQueryAdapter";
-import ContractService from "@service/contract/ContractService";
 import { GetCouponFromOrderedListAtQueryError } from "./error/GetCouponFromOrderedListAtQueryError";
 import {
   GetCouponFromOrderedListAtQuery,
   GetCouponFromOrderedListAtQueryResponse,
 } from "./GetCouponFromOrderedListAtQuery";
 import EvmAddress from "@domain/context/contract/EvmAddress";
+import AccountService from "@service/account/AccountService";
 
 @QueryHandler(GetCouponFromOrderedListAtQuery)
 export class GetCouponFromOrderedListAtQueryHandler implements IQueryHandler<GetCouponFromOrderedListAtQuery> {
   constructor(
     @lazyInject(RPCQueryAdapter)
     private readonly queryAdapter: RPCQueryAdapter,
-    @lazyInject(ContractService)
-    private readonly contractService: ContractService,
+    @lazyInject(AccountService)
+    private readonly accountService: AccountService,
   ) {}
 
   async execute(query: GetCouponFromOrderedListAtQuery): Promise<GetCouponFromOrderedListAtQueryResponse> {
     try {
       const { securityId, pos } = query;
 
-      const securityEvmAddress: EvmAddress = await this.contractService.getContractEvmAddress(securityId);
+      const securityEvmAddress: EvmAddress = await this.accountService.getAccountEvmAddress(securityId);
 
       const res = await this.queryAdapter.getCouponFromOrderedListAt(securityEvmAddress, pos);
 
