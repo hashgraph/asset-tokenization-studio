a55a6262386a78370bf63d1f9ce166de988f0665
Ruben Martinez <ruben.martinez@io.builders>
2026-02-18 10:19:36 +0100

    fix: pr changes
    Signed-off-by: Ruben Martinez <ruben.martinez@io.builders>

--- packages/ats/sdk/src/app/usecase/command/kpis/addKpiData/AddKpiDataCommandHandler.ts
+++ packages/ats/sdk/src/app/usecase/command/kpis/addKpiData/AddKpiDataCommandHandler.ts
@@ -7,12 +7,19 @@ import TransactionService from "@service/transaction/TransactionService";
 import { lazyInject } from "@core/decorator/LazyInjectDecorator";
 import EvmAddress from "@domain/context/contract/EvmAddress";
 import { AddKpiDataCommandError } from "./error/AddKpiDataCommandError";
+import ValidationService from "@service/validation/ValidationService";
+import { SecurityRole } from "@domain/context/security/SecurityRole";
+import AccountService from "@service/account/AccountService";
 
 @CommandHandler(AddKpiDataCommand)
 export class AddKpiDataCommandHandler implements ICommandHandler<AddKpiDataCommand> {
   constructor(
     @lazyInject(TransactionService)
     private readonly transactionService: TransactionService,
+    @lazyInject(ValidationService)
+    private readonly validationService: ValidationService,
+    @lazyInject(AccountService)
+    private readonly accountService: AccountService,
   ) {}
 
   async execute(command: AddKpiDataCommand): Promise<AddKpiDataCommandResponse> {
@@ -20,6 +27,11 @@ export class AddKpiDataCommandHandler implements ICommandHandler<AddKpiDataComma
       const { securityId, date, value, project } = command;
 
       const transactionAdapter = this.transactionService.getHandler();
+      const account = this.accountService.getCurrentAccount();
+
+      await this.validationService.checkPause(securityId);
+      await this.validationService.checkRole(SecurityRole._KPI_MANAGER_ROLE, account.id.toString(), securityId);
+
       const result = await transactionAdapter.addKpiData(
         new EvmAddress(securityId),
         date,
