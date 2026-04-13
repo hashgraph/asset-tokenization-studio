4c36ed0186693f327e509f44f7f77815ddfb6ddb
mamoralesiob <miguelangel@io.builders>
2026-04-13 18:26:45 +0200

    fix: fix a mass-payout contracts error
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/mass-payout/contracts/contracts/test/testAsset/interfaces/IAssetMock.sol
+++ packages/mass-payout/contracts/contracts/test/testAsset/interfaces/IAssetMock.sol
@@ -4,10 +4,9 @@ pragma solidity 0.8.22;
 // solhint-disable max-line-length
 
 import { IBond } from "@hashgraph/asset-tokenization-contracts/contracts/facets/layer_2/bond/IBond.sol";
-import { IBondRead } from "@hashgraph/asset-tokenization-contracts/contracts/facets/layer_2/bond/IBondRead.sol";
 import { IERC20 } from "@hashgraph/asset-tokenization-contracts/contracts/facets/layer_1/ERC1400/ERC20/IERC20.sol";
 import { IEquity } from "@hashgraph/asset-tokenization-contracts/contracts/facets/layer_2/equity/IEquity.sol";
 
-interface IAssetMock is IBond, IBondRead, IEquity, IERC20 {
+interface IAssetMock is IBond, IEquity, IERC20 {
     error NotImplemented();
 }
