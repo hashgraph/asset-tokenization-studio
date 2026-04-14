f995a144551ba9de740d2b363756e26c732b96a1
mamoralesiob <miguelangel@io.builders>
2026-04-14 16:25:19 +0200

    fix: solidity v0.8.27 to v0.8.28 en hardhat.config.ts
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/ats/contracts/hardhat.config.ts
+++ packages/ats/contracts/hardhat.config.ts
@@ -17,7 +17,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.27",
+        version: "0.8.28",
         settings: {
           optimizer: {
             enabled: true,
