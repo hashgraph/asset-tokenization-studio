d73e06b4ab86bfd5fcde088f457969be52833b07
mamoralesiob <miguelangel@io.builders>
2026-05-06 09:15:50 +0200

    refactor: operator facet events (#1054)
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/ats/contracts/contracts/facets/operator/Operator.sol
+++ packages/ats/contracts/contracts/facets/operator/Operator.sol
@@ -19,6 +19,7 @@ abstract contract Operator is IOperator, Modifiers {
         address _operator
     ) external override onlyUnpaused onlyCompliant(EvmAccessors.getMsgSender(), _operator, false) {
         ERC1410StorageWrapper.authorizeOperator(_operator);
+        emit OperatorAuthorized(_operator, EvmAccessors.getMsgSender());
     }
 
     /// @inheritdoc IOperator
@@ -32,6 +33,7 @@ abstract contract Operator is IOperator, Modifiers {
         onlyCompliant(EvmAccessors.getMsgSender(), _operator, false)
     {
         ERC1410StorageWrapper.revokeOperator(_operator);
+        emit OperatorRevoked(_operator, EvmAccessors.getMsgSender());
     }
 
     /// @inheritdoc IOperator
