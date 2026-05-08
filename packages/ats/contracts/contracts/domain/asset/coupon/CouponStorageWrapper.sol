e28683963eda0cec6bcbcb7581e31ac2354d0967
mamoralesiob <miguelangel@io.builders>
2026-05-08 16:14:17 +0200

    refactor: improve for loop
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/ats/contracts/contracts/domain/asset/coupon/CouponStorageWrapper.sol
+++ packages/ats/contracts/contracts/domain/asset/coupon/CouponStorageWrapper.sol
@@ -292,13 +292,19 @@ library CouponStorageWrapper {
 
         if (getCouponFromOrderedListAt(0) == couponID) return (0);
 
-        orderedListLength--;
-        uint256 previousCouponId = 0;
+        unchecked {
+            orderedListLength--;
+        }
+        uint256 previousCouponId;
 
-        for (uint256 index = 0; index < orderedListLength; index++) {
-            previousCouponId = getCouponFromOrderedListAt(index);
-            uint256 couponId = getCouponFromOrderedListAt(index + 1);
+        for (uint256 i; i < orderedListLength; ) {
+            previousCouponId = getCouponFromOrderedListAt(i);
+            uint256 couponId = getCouponFromOrderedListAt(i + 1);
             if (couponId == couponID) return previousCouponId;
+
+            unchecked {
+                ++i;
+            }
         }
         return 0;
     }
