9ed355c1ff799fa36ef5470882f2dcde9c12ce15
jaime-iobermudez <jaime.bermudez@io.builders>
2026-05-08 13:43:20 +0200

    fix: find-073
    Signed-off-by: jaime-iobermudez <jaime.bermudez@io.builders>

--- packages/ats/contracts/contracts/domain/asset/ERC20PermitStorageWrapper.sol
+++ packages/ats/contracts/contracts/domain/asset/ERC20PermitStorageWrapper.sol
@@ -37,7 +37,7 @@ library ERC20PermitStorageWrapper {
 
         uint256 currentNonce = NonceStorageWrapper.getNonceFor(owner);
 
-        NonceStorageWrapper.setNonceFor(currentNonce + 1, owner);
+        NonceStorageWrapper.setNonceFor(owner);
         // solhint-disable-next-line func-name-mixedcase
         address signer = ECDSA.recover(
             ECDSA.toTypedDataHash(
