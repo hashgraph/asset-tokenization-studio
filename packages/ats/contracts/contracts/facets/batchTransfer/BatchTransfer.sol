ef1ecc4f28a4803b73761faaa26531abe44636dd
mamoralesiob <miguelangel@io.builders>
2026-05-13 12:26:42 +0200

    fix: audit FIND-090
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/ats/contracts/contracts/facets/batchTransfer/BatchTransfer.sol
+++ packages/ats/contracts/contracts/facets/batchTransfer/BatchTransfer.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import { IBatchTransfer } from "./IBatchTransfer.sol";
 import { Modifiers } from "../../services/Modifiers.sol";
+import { ExternalListManagementStorageWrapper } from "../../domain/core/ExternalListManagementStorageWrapper.sol";
 import { ERC1594StorageWrapper } from "../../domain/asset/ERC1594StorageWrapper.sol";
 import { TokenCoreOps } from "../../domain/orchestrator/TokenCoreOps.sol";
 import { EvmAccessors } from "../../infrastructure/utils/EvmAccessors.sol";
@@ -34,6 +35,7 @@ abstract contract BatchTransfer is IBatchTransfer, Modifiers {
     {
         uint256 length = _toList.length;
         for (uint256 i; i < length; ) {
+            ExternalListManagementStorageWrapper.checkValidAddress(_toList[i]);
             ERC1594StorageWrapper.checkIdentity(address(0), _toList[i]);
             ERC1594StorageWrapper.checkCompliance(address(0), _toList[i], false);
             unchecked {
