1ae06f8ff145ff0f9d612507ce3ba59320b7a6fd
Alberto Molina <alberto@io.builders>
2026-05-14 15:27:48 +0200

    fix: gas limit changes removed
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- packages/ats/contracts/scripts/infrastructure/constants.ts
+++ packages/ats/contracts/scripts/infrastructure/constants.ts
@@ -169,13 +169,13 @@ export const DEFAULT_BATCH_SIZE = 15;
  */
 export const GAS_LIMIT = {
   max: 15_000_000,
-  default: 5_000_000,
+  default: 3_000_000,
   low: 1_000_000,
-  high: 15_000_000,
+  high: 10_000_000,
   /** 2000 Gwei ? must be set alongside gasLimit to skip eth_estimateGas on Hedera */
   gasPrice: 2_000_000_000_000n,
   initialize: {
-    businessLogicResolver: 15_000_000,
+    businessLogicResolver: 8_000_000,
     factory: 300_000,
   },
   proxyAdmin: {
@@ -183,7 +183,7 @@ export const GAS_LIMIT = {
   },
   businessLogicResolver: {
     getStaticResolverKey: 60_000,
-    registerBusinessLogics: 10_000_000,
+    registerBusinessLogics: 7_800_000,
     createConfiguration: 15_000_000,
   },
 } as const;
