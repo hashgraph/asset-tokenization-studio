a8195cde96e098f82313287219c0a2d268e3e419
mamoralesiob <miguelangel@io.builders>
2026-05-19 12:27:10 +0200

    fix: audit FIND-135
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

---
+++ packages/ats/contracts/contracts/test/mocks/revocationRegistry/RevertingRevocationRegistry.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract RevertingRevocationRegistry {
+    function revoked(address, string calldata) external pure returns (bool) {
+        revert("registry unavailable");
+    }
+}
