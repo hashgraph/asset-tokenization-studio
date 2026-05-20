f763a393f0e2fd5035b97a3662f4d19ad5e5472d
mamoralesiob <miguelangel@io.builders>
2026-05-20 09:57:12 +0200

    chore: disable linting in RevertingRevocationRegistry.sol
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/ats/contracts/contracts/test/mocks/revocationRegistry/RevertingRevocationRegistry.sol
+++ packages/ats/contracts/contracts/test/mocks/revocationRegistry/RevertingRevocationRegistry.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
+
+/// solhint-disable
 pragma solidity >=0.8.0 <0.9.0;
 
 contract RevertingRevocationRegistry {
@@ -6,3 +8,4 @@ contract RevertingRevocationRegistry {
         revert("registry unavailable");
     }
 }
+/// solhint-enable
