7f4a96466482512b6831fdb1d59a2ced4c038bef
Marcos Serradilla Diez <marcos@io.builders>
2026-06-03 12:30:39 +0200

    refactor(contracts+scripts): consolidate TransferAndLock facet varian… (#1254)
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>

--- packages/ats/contracts/contracts/facets/transferAndLock/TransferAndLockFacet.sol
+++ packages/ats/contracts/contracts/facets/transferAndLock/TransferAndLockFacet.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import { RESOLVER_KEY_TRANSFER_AND_LOCK } from "../../IAsset.sol";
+import { RESOLVER_KEY_TRANSFER_AND_LOCK } from "../IAsset.sol";
 
-import { TransferAndLockFacetBase } from "../TransferAndLockFacetBase.sol";
+import { TransferAndLockFacetBase } from "./TransferAndLockFacetBase.sol";
 
 contract TransferAndLockFacet is TransferAndLockFacetBase {
     function getStaticResolverKey() external pure override returns (bytes32 staticResolverKey_) {
