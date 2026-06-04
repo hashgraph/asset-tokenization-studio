c06db90c04433ce85997c5d96aa289550a1d644f
mamoralesiob <miguelangel@io.builders>
2026-06-04 09:49:44 +0200

    refactor: loan was moved from layer_2 to facets (#1257)
    Signed-off-by: mamoralesiob <miguelangel@io.builders>

--- packages/ats/contracts/contracts/facets/loan/LoanFacet.sol
+++ packages/ats/contracts/contracts/facets/loan/LoanFacet.sol
@@ -2,21 +2,26 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import { ILoan, RESOLVER_KEY_LOAN } from "./ILoan.sol";
-import { IStaticFunctionSelectors } from "../../../infrastructure/proxy/IStaticFunctionSelectors.sol";
-import { Bytes4Builder } from "../../../infrastructure/proxy/Bytes4Builder.sol";
+import { IStaticFunctionSelectors } from "../../infrastructure/proxy/IStaticFunctionSelectors.sol";
+import { Bytes4Builder } from "../../infrastructure/proxy/Bytes4Builder.sol";
 import { Loan } from "./Loan.sol";
 
 /**
- * @title LoanFacet
- * @notice Facet for loan lifecycle operations
- * @dev Concrete facet exposing loan creation, state management, and redemption
- * @author Hashgraph
+ * @title  LoanFacet
+ * @author Asset Tokenization Studio Team
+ * @notice Diamond facet that exposes loan metadata management to the proxy.
+ * @dev    Selectors exposed:
+ *         - `initializeLoan`
+ *         - `setLoanDetails`
+ *         - `getLoanDetails`
  */
 contract LoanFacet is Loan, IStaticFunctionSelectors {
+    /// @inheritdoc IStaticFunctionSelectors
     function getStaticResolverKey() external pure override returns (bytes32 staticResolverKey_) {
         staticResolverKey_ = RESOLVER_KEY_LOAN;
     }
 
+    /// @inheritdoc IStaticFunctionSelectors
     function getStaticFunctionSelectors() external pure override returns (bytes4[] memory) {
         return
             Bytes4Builder.build(
@@ -26,6 +31,7 @@ contract LoanFacet is Loan, IStaticFunctionSelectors {
             );
     }
 
+    /// @inheritdoc IStaticFunctionSelectors
     function getStaticInterfaceIds() external pure override returns (bytes4[] memory) {
         return Bytes4Builder.build(type(ILoan).interfaceId);
     }
